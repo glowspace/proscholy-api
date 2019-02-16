b2cd585be11ca7cb699a66666dba53b4b7326060
Michael Dojčár <michaeldojcar@gmail.com>
2019-02-16 19:16:31 +0100

    search organized by model type

--- resources/views/client/search_results.blade.php
+++ resources/views/client/search_results.blade.php
@@ -8,15 +8,28 @@
     <div class="content-padding">
         <h1>Výsledky vyhledávání pro {{$phrase}}</h1>
 
+        <h2>Písně</h2>
+
         <table class="table">
-            {{-- implements interface ISearchResult --}}
-            @foreach($search_results as $result)
+            @foreach($song_lyrics as $song_lyric)
                 <tr>
-                    <td>{{$result->getSearchTitle()}}</td>
+                    <td>
+                        <a href="{{$song_lyric->id}}">{{$song_lyric->getSearchTitle()}}</a> - {{$song_lyric->getSearchText()}}
+                    </td>
                 </tr>
             @endforeach
         </table>
 
-        {{ $search_results->links() }}
+        <h2>Autoři</h2>
+
+        <table class="table">
+            @foreach($authors as $author)
+                <tr>
+                    <td>
+                        <a href="{{$author->id}}">{{$author->getSearchTitle()}}</a> - {{$author->getSearchText()}}
+                    </td>
+                </tr>
+            @endforeach
+        </table>
     </div>
 @endsection
