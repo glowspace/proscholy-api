98ca7f0cabf9ccc4b6a86a9922100142b3118fbb
Michael Dojčár <michaeldojcar@gmail.com>
2019-02-16 17:54:21 +0100

    css yield class fix (background isolation)

--- resources/views/client/home.blade.php
+++ resources/views/client/home.blade.php
@@ -1,15 +1,18 @@
 @extends('layout.layout')
 
 @section('content')
-    <div class="logo-wrapper">
-        <div class="logo"></div>
-        <span class="caption material-shadow-text">Zpěvník</span>
-    </div>
-    <div class="search-wrapper">
-        <input class="search-home  material-shadow"
-               placeholder="Zadejte název písně (třeba Ať požehnán je Bůh)">
-        <button type="submit" class="search-submit material-shadow">
-            <i class="fa fa-search"></i>
-        </button>
+    <div class="background-home">
+
+        <div class="logo-wrapper">
+            <div class="logo"></div>
+            <span class="caption material-shadow-text">Zpěvník</span>
+        </div>
+        <div class="search-wrapper">
+            <input class="search-home  material-shadow"
+                   placeholder="Zadejte název písně (třeba Ať požehnán je Bůh)">
+            <button type="submit" class="search-submit material-shadow">
+                <i class="fa fa-search"></i>
+            </button>
+        </div>
     </div>
 @endsection
