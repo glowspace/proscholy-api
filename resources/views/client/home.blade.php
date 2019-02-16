493c3724bde1d727b1cb31c7e8af1e76bf8c9773
Michael Dojčár <michaeldojcar@gmail.com>
2019-02-16 17:22:14 +0100

    homepage view, css

--- resources/views/client/home.blade.php
+++ resources/views/client/home.blade.php
@@ -1,17 +1,15 @@
 @extends('layout.layout')
 
 @section('content')
-
-            <div class="logo-wrapper">
-                <div class="logo"></div>
-                <span class="caption material-shadow-text">Zpěvník</span>
-            </div>
-            <div class="search-wrapper">
-                <input class="search-home  material-shadow"
-                       placeholder="Zadejte název písně (třeba Ať požehnán je Bůh)">
-                <button type="submit" class="search-submit material-shadow">
-                    <i class="fa fa-search"></i>
-                </button>
-            </div>
-
+    <div class="logo-wrapper">
+        <div class="logo"></div>
+        <span class="caption material-shadow-text">Zpěvník</span>
+    </div>
+    <div class="search-wrapper">
+        <input class="search-home  material-shadow"
+               placeholder="Zadejte název písně (třeba Ať požehnán je Bůh)">
+        <button type="submit" class="search-submit material-shadow">
+            <i class="fa fa-search"></i>
+        </button>
+    </div>
 @endsection
