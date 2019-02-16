21bba952ce614b290eb6be5d70540608361aaf97
Michael Dojčár <michaeldojcar@gmail.com>
2019-02-16 20:59:10 +0100

    menu css

--- resources/views/client/home.blade.php
+++ resources/views/client/home.blade.php
@@ -1,8 +1,14 @@
 @extends('layout.layout')
 
+@section('navbar')
+    <button type="button" class="btn btn-secondary">Vyhledávání</button>
+    <button type="button" class="btn btn-secondary">Kdo jsme</button>
+    <button type="button" class="btn btn-secondary">Facebook</button>
+    <button type="button" class="btn btn-secondary">Instagram</button>
+@endsection
+
 @section('content')
     <div class="background-home">
-
         <div class="logo-wrapper">
             <div class="logo"></div>
             <span class="caption material-shadow-text">Zpěvník</span>
