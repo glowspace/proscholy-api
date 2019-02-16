4f0002fcc3ef352bda3eae8dc3dd67ce71d1054d
Miroslav Sery <athes01@gmail.com>
2019-02-16 17:04:38 +0100

    added songText method to SongLyricsController

--- app/Http/Controllers/Client/SongLyricsController.php
+++ app/Http/Controllers/Client/SongLyricsController.php
@@ -9,7 +9,21 @@ use Illuminate\Http\Request;
 
 class SongLyricsController extends Controller
 {
-    public function render($id)
+    // public function render($id)
+    // {
+    //     $song_l = SongLyric::findOrFail($id);
+    //     $song_l->visits += 1;
+    //     $song_l->save();
+
+    //     foreach ($song_l->authors as $author) {
+    //         $author->visits += 1;
+    //         $author->save();
+    //     }
+
+    //     return view('song_lyrics', compact('song_l'));
+    // }
+
+    public function songText($id)
     {
         $song_l = SongLyric::findOrFail($id);
         $song_l->visits += 1;
@@ -20,6 +34,6 @@ class SongLyricsController extends Controller
             $author->save();
         }
 
-        return view('song_lyrics', compact('song_l'));
+        return view('client.song_lyrics', compact('song_l'));
     }
 }
