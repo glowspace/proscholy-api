a6e25be112ba5868fcf611a0af835a491f7e8a04
Michael Dojčár <michaeldojcar@gmail.com>
2019-02-16 18:54:28 +0100

    scrollbar shows automatically

--- resources/views/client/home.blade.php
+++ resources/views/client/home.blade.php
@@ -10,8 +10,8 @@
         <div class="search-wrapper">
             <form method="POST" action="{{route('client.search')}}">
                 @csrf
-                <input class="search-home  material-shadow" name="query"
-                       placeholder="Zadejte název písně (třeba Ať požehnán je Bůh)">
+                <input class="search-home material-shadow" name="query"
+                       placeholder="Zadejte název písně (třeba Ať požehnán je Bůh)" autofocus>
                 <button type="submit" class="search-submit material-shadow">
                     <i class="fa fa-search"></i>
                 </button>
