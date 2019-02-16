6d95eb274a5c8770c501cfaced5780d6b6524116
Michael Dojčár <michaeldojcar@gmail.com>
2019-02-16 18:28:16 +0100

    search basic listing

--- resources/views/client/search_results.blade.php
+++ resources/views/client/search_results.blade.php
@@ -1,9 +1,21 @@
 @extends('layout.layout')
 
-@section('content')
-    <h1>Výsledky vyhledávání {{$phrase}}</h1>
+@section('navbar')
 
-    <table class="table">
-        
-    </table>
+@endsection
+
+@section('content')
+    <div class="content-padding">
+
+
+        <h1>Výsledky vyhledávání {{$phrase}}</h1>
+
+        <table class="table">
+            @foreach($song_lyrics as $song)
+                <tr>
+                    <td>{{$song->name}}</td>
+                </tr>
+            @endforeach
+        </table>
+    </div>
 @endsection
