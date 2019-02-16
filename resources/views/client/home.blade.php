9fa74f91dc7b28776c1d6de0b2ada735d0d618ba
Michael Dojčár <michaeldojcar@gmail.com>
2019-02-16 16:02:19 +0100

    font of logo title changed to Cocogoose Pro

--- resources/views/client/home.blade.php
+++ resources/views/client/home.blade.php
@@ -1,65 +1,19 @@
 @extends('layout.layout')
 
 @section('content')
-
     <div class="row">
-        <div class="col-sm-4">
-            <h4>Nejnavštěvovanější písně</h4>
-            @foreach($top_songs as $song_l)
-                {{-- @if(isset($song_l->song_id))
-                    <a href="{{route('translation.single', ['id'=> $song->id])}}">{{$song->name}}</a>
-                    ({{$song->visits}}x)<br>
-                @else
-                    <a href="{{route('song.single', ['id'=> $song->id])}}">{{$song->name}}</a> ({{$song->visits}}x)<br>
-                @endif --}}
-                <a href="{{ route('song_lyrics.single', ['id'=> $song_l->id]) }}">{{ $song_l->name }}</a>
-                ({{ $song_l->visits }}x)<br>
-            @endforeach
+        <div class="col-md-2 sidebar bg-dark material-shadow"></div>
+        <div class="col-md-10 background-home">
+            <div class="logo-wrapper">
+                    <span class="caption">
+                        <img class="logo-image" src="{{asset('img/logo_bubble.svg')}}" height="180"
+                             alt="">Zpěvník</span>
+            </div>
+            <div class="search-wrapper">
+                <input class="search-home  material-shadow"
+                       placeholder="Zadejte název písně (třeba Ať požehnán je Bůh)">
+                <input type="submit" class="search-submit">
+            </div>
         </div>
-        <div class="col-sm-4">
-            <script>
-                (function () {
-                    var cx = '007397572785069727063:fb8ec8z7fcu';
-                    var gcse = document.createElement('script');
-                    gcse.type = 'text/javascript';
-                    gcse.async = true;
-                    gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
-                    var s = document.getElementsByTagName('script')[0];
-                    s.parentNode.insertBefore(gcse, s);
-                })();
-            </script>
-
-            <h3>Vyhledávání</h3>
-            <gcse:search></gcse:search>
-
-            <h3>Rejstřík</h3>
-
-            <a class="btn btn-primary" style="width: 100%; margin-bottom:10px;" href="{{route('song.list')}}">Abecední
-                seznam písní</a><br>
-            <a class="btn btn-primary" style="width: 100%; margin-bottom:10px;" href="{{route('author.list')}}">Seznam
-                autorů</a>
-
-
-            <h4>Zpěvník v počtech</h4>
-
-            <p>Písně: <b>{{$songs_count}} <i class="fas fa-music"></i></b></p>
-            {{-- <p>Překlady: <b>{{$translations_count}} <i class="fas fa-language"></i></b></p> --}}
-            <p>Autoři: <b>{{$authors_count}} <i class="fas fa-user"></i></b></p>
-            <p>Videa: <b>{{$externals_count}} <i class="fas fa-external"></i></b></p>
-
-            {{-- <p>{{$lyrics_percentage}}% písní má přidaný text</p> --}}
-
-            {{--<a class="btn btn-primary" style="width: 100%; margin-bottom:10px;" href="{{route('author.list')}}">--}}
-            {{--Něco mi tu chybí!</a>--}}
-        </div>
-        <div class="col-sm-4">
-            <h4>Nejnavštěvovanější autoři</h4>
-            @foreach($top_authors as $author)
-                <a href="{{route('author.single', ['id'=> $author->id])}}">{{$author->name}}</a> ({{$author->visits}}x)
-                <br>
-            @endforeach
-        </div>
-
     </div>
-
 @endsection
--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -49,19 +49,7 @@
     </nav>
 
     <div class="container-fluid">
-        <div class="row">
-            <div class="col-md-2 sidebar bg-dark material-shadow"></div>
-            <div class="col-md-10 background-home">
-                <div class="logo-wrapper">
-                    <span class="caption">
-                        <img class="logo-image" src="{{asset('img/logo_bubble.svg')}}" height="150">Zpěvník</span>
-                </div>
-                <div class="search-wrapper">
-                    <input class="search-home  material-shadow"
-                           placeholder="Zadejte název písně (třeba Ať požehnán je Bůh)">
-                </div>
-            </div>
-        </div>
+        @yield('content')
     </div>
 
     @yield('scripts')
