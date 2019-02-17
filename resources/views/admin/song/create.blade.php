0fefe7b2c104293e874a6a334fea118cdbf0488e
Miroslav Sery <athes01@gmail.com>
2019-02-17 06:20:04 +0100

    made nicer admin

--- resources/views/admin/song/create.blade.php
+++ resources/views/admin/song/create.blade.php
@@ -8,8 +8,8 @@
             @csrf
             <input required class="form-control" autofocus name="name" placeholder="Název písně"><br>
 
-            <button type="submit" name="redirect" value="create">Uložit</button>
-            <button type="submit" name="redirect" value="edit">Uložit a upravit</button>
+            <button type="submit" class="btn btn-outline-primary" name="redirect" value="create">Uložit</button>
+            <button type="submit" class="btn btn-outline-primary" name="redirect" value="edit">Uložit a upravit</button>
         </form>
     </div>
 @endsection
