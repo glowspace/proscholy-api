43401633ab35156e67ad8d92ef8781e8641b3a51
Michael Dojčár <michaeldojcar@gmail.com>
2019-02-17 09:42:16 +0100

    author type, empty result search

--- app/Author.php
+++ app/Author.php
@@ -9,20 +9,20 @@ use Illuminate\Support\Arr;
 /**
  * App\Author
  *
- * @property int                                                                  $id
- * @property string|null                                                          $name
- * @property string|null                                                          $url
- * @property string|null                                                          $ytchannel
- * @property string|null                                                          $description
- * @property string|null                                                          $email
- * @property string|null                                                          $password
- * @property int|null                                                             $type
- * @property \Carbon\Carbon|null                                                  $created_at
- * @property \Carbon\Carbon|null                                                  $updated_at
- * @property-read \Illuminate\Database\Eloquent\Collection|\App\Author[]          $isMemberOf
- * @property-read \Illuminate\Database\Eloquent\Collection|\App\Author[]          $members
+ * @property int                                                            $id
+ * @property string|null                                                    $name
+ * @property string|null                                                    $url
+ * @property string|null                                                    $ytchannel
+ * @property string|null                                                    $description
+ * @property string|null                                                    $email
+ * @property string|null                                                    $password
+ * @property int|null                                                       $type
+ * @property \Carbon\Carbon|null                                            $created_at
+ * @property \Carbon\Carbon|null                                            $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Author[]    $isMemberOf
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Author[]    $members
  * @property-read \Illuminate\Database\Eloquent\Collection|\App\SongLyric[] $songLyrics
- * @property-read \Illuminate\Database\Eloquent\Collection|\App\External[]        $externals
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\External[]  $externals
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Author whereCreatedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Author whereDescription($value)
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Author whereEmail($value)
@@ -34,9 +34,9 @@ use Illuminate\Support\Arr;
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Author whereUrl($value)
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Author whereYtchannel($value)
  * @mixin \Eloquent
- * @property int|null $visits
+ * @property int|null                                                       $visits
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Author whereVisits($value)
- * @property-read \Illuminate\Database\Eloquent\Collection|\App\Author[] $memberships
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Author[]    $memberships
  */
 class Author extends Model implements ISearchResult
 {
@@ -44,7 +44,15 @@ class Author extends Model implements ISearchResult
     use Searchable;
     protected $fillable = ['name', 'description', 'email', 'url', 'type'];
 
-    
+    public $type_string
+        = [
+            0 => 'autor',
+            1 => 'hudební uskupení',
+            2 => 'schola',
+            3 => 'kapela',
+            4 => 'sbor',
+        ];
+
 
     public function songLyrics()
     {
@@ -53,15 +61,15 @@ class Author extends Model implements ISearchResult
 
     public function songOriginalLyrics()
     {
-        return $this->songLyrics()->where('is_original',true);
+        return $this->songLyrics()->where('is_original', true);
     }
 
     public function songNotOriginalLyrics()
     {
-        return $this->songLyrics()->where('is_original',false);
+        return $this->songLyrics()->where('is_original', false);
     }
 
-    public function members()   
+    public function members()
     {
         return $this->belongsToMany(Author::class,
             'author_membership',
@@ -112,6 +120,6 @@ class Author extends Model implements ISearchResult
 
     public function getSearchText()
     {
-        return $this->description;
+        return $this->type_string[$this->type];
     }
 }
--- resources/views/client/search_results.blade.php
+++ resources/views/client/search_results.blade.php
@@ -11,47 +11,49 @@
         <form method="POST" action="{{route('client.search')}}">
             @csrf
             <input class="form-control search-basic" name="query"
-                   placeholder="Zadejte název písně (třeba Ať požehnán je Bůh)" value="{{$phrase}}" autofocus>
+                   placeholder="Zadejte název písně, část textu nebo jméno autora" value="{{$phrase}}" autofocus>
             <button type="submit" class="btn btn-primary">
                 <i class="fa fa-search"></i>
             </button>
         </form>
 
-        <h2>Písně</h2>
+        @if(isset($phrase))
+            <h2>Písně</h2>
 
-        <table class="table">
-            @forelse($song_lyrics as $song_lyric)
-                <tr>
-                    <td>
-                        <i class="fas fa-music"></i> <a
-                                href="{{route('client.song.text',$song_lyric)}}">{{$song_lyric->getSearchTitle()}}</a>
-                    </td>
-                </tr>
-            @empty
-                <tr>
-                    <td>
-                        <i>Žádná píseň s tímto názvem nebyla nalezena.</i>
-                    </td>
-                </tr>
-            @endforelse
-        </table>
+            <table class="table">
+                @forelse($song_lyrics as $song_lyric)
+                    <tr>
+                        <td>
+                            <i class="fas fa-music"></i> <a
+                                    href="{{route('client.song.text',$song_lyric)}}">{{$song_lyric->getSearchTitle()}}</a>
+                        </td>
+                    </tr>
+                @empty
+                    <tr>
+                        <td>
+                            <i>Žádná píseň s tímto názvem nebyla nalezena.</i>
+                        </td>
+                    </tr>
+                @endforelse
+            </table>
 
-        <h2>Autoři</h2>
+            <h2>Autoři</h2>
 
-        <table class="table">
-            @forelse($authors as $author)
-                <tr>
-                    <td>
-                        <a href="{{$author->id}}">{{$author->getSearchTitle()}}</a> - {{$author->getSearchText()}}
-                    </td>
-                </tr>
-            @empty
-                <tr>
-                    <td>
-                        <i>Žádný autor nebyl nalezen.</i>
-                    </td>
-                </tr>
-            @endforelse
-        </table>
-    </div>
+            <table class="table">
+                @forelse($authors as $author)
+                    <tr>
+                        <td>
+                            <a href="{{route('client.author',$author)}}">{{$author->getSearchTitle()}}</a> - {{$author->getSearchText()}}
+                        </td>
+                    </tr>
+                @empty
+                    <tr>
+                        <td>
+                            <i>Žádný autor nebyl nalezen.</i>
+                        </td>
+                    </tr>
+                @endforelse
+            </table>
+        </div>
+    @endif
 @endsection
