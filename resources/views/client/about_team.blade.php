91b24f94a0333c9cd675ad1459d1fbd9775ec3d4
Michael Dojčár <michaeldojcar@gmail.com>
2019-02-17 18:54:54 +0100

    added about team route and controller method

--- resources/views/client/about_team.blade.php
+++ resources/views/client/about_team.blade.php
@@ -1,23 +1,89 @@
 @extends('layout.master')
 
 @section('navbar')
-    <a class="btn btn-secondary" href="{{route('client.home')}}">
-        <i class="fas fa-search"></i> Nové vyhledávání
-    </a>
+    @include('client.components.menu_main')
 @endsection
 
 @section('content')
     <div class="content-padding">
-        <h1>Team</h1>
+        <h1>Redakce ProScholy.cz</h1>
 
-        <p>Zpěvník připravuje</p>
+        <p>Chcete se podílet na tvorbě zpěnvníku?
+            Máte tipy na nové funkce nebo nový repertoár?
+            Můžete nám napsat na email redakce@proscholy.cz
+            nebo na naší FB stránce.</p>
+
+        <h3>Náš redakční tým</h3>
 
         <div class="row">
-            <div class="col-3">
+            <div class="col-sm-3">
                 <div class="card">
                     <div class="card-body">
                         <h5 class="card-title">Michael Dojčár</h5>
-                        {{--<p class="card-text"</p>--}}
+                        <p class="card-text">koordinace projektu, IT</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-3">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Miroslav Šerý</h5>
+                        <p class="card-text">vedoucí vývojář</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-3">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Benjamín Tichý</h5>
+                        <p class="card-text">grafika</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-3">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Matěj Kulišťák</h5>
+                        <p class="card-text">redakční tým</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-3">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Zuzana Haikerová</h5>
+                        <p class="card-text">redakční tým</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-3">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Václav Šablatura</h5>
+                        <p class="card-text">redakční tým, odborný konzultant</p>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="col-3">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Petra Kalousková</h5>
+                        <p class="card-text">PR tým</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-3">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Emma Kasanová</h5>
+                        <p class="card-text">PR tým</p>
                     </div>
                 </div>
             </div>
--- resources/views/client/components/menu_main.blade.php
+++ resources/views/client/components/menu_main.blade.php
@@ -1,9 +1,9 @@
 <a class="btn btn-secondary" href="{{route('client.home')}}">
     <i class="fas fa-search"></i> Vyhledávání
 </a>
-{{--<a class="btn btn-secondary">--}}
-    {{--<i class="fas fa-users"></i> Kdo jsme--}}
-{{--</a>--}}
+<a class="btn btn-secondary" href="{{route('client.team')}}">
+    <i class="fas fa-users"></i> Redakce
+</a>
 {{--<a class="btn btn-secondary">--}}
     {{--<i class="fab fa-facebook"></i> Facebook--}}
 {{--</a>--}}
