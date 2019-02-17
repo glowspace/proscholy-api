427907ee9d50951fd6d5e55d1fb8b9c915cbb205
Michael Dojčár <michaeldojcar@gmail.com>
2019-02-17 18:03:01 +0100

    admin dashboard added team info

--- resources/views/admin/dash.blade.php
+++ resources/views/admin/dash.blade.php
@@ -15,7 +15,19 @@
                         <td>Autor:</td>
                         <td>Miroslav Šerý, Michael Dojčár</td>
                     </tr>
+                    <tr>
+                        <td>Krizový telefon <br>(urgent. dotazy, závady)</td>
+                        <td class="text-danger">+420 734 791 909</td>
+                    </tr>
                 </table>
+            </div>
+            <div class="col-sm-7">
+                <h3>Důležité info</h3>
+
+                <div class="card">
+                    <div class="card-header">Hlavní komunikační kanál</div>
+                    <div class="card-body">Slack <a href="https://proscholy.slack.com">proscholy.slack.com</a></div>
+                </div>
             </div>
         </div>
     </div>
