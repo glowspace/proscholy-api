c1bff2e26b5c88a559ceb854b694c9f30b17a69f
Miroslav Sery <athes01@gmail.com>
2019-02-18 10:52:57 +0100

    fixed bad admin redirecting

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -28,7 +28,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/admin/manage';
+    protected $redirectTo = '/admin';
 
     /**
      * Create a new controller instance.
