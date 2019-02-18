cf4639700979d8f5222abf893c119bf13d6bcd36
Michael Dojčár <michaeldojcar@gmail.com>
2019-02-18 22:55:19 +0100

    client song view import fix

--- resources/views/client/song/song_scores.blade.php
+++ resources/views/client/song/song_scores.blade.php
@@ -57,15 +57,4 @@
             src="https://code.jquery.com/jquery-3.3.1.min.js"
             integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
             crossorigin="anonymous"></script>
-
-    @include('scripts.chordpro_parse')
-
-    <script>
-        $(document).ready(function () {
-            let lyrics = document.getElementById('lyrics');
-            let lyrics_source = document.getElementById('lyrics').innerHTML;
-
-            lyrics.innerHTML = parseChordPro(lyrics_source, 0);
-        });
-    </script>
 @endpush
