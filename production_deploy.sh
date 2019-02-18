b2a4bbe375eda00f42e5c3a52bd9c25bf4de59e3
Michael Dojčár <michaeldojcar@gmail.com>
2019-02-18 12:40:59 +0100

    added fast bash deploy command

--- production_deploy.sh
+++ production_deploy.sh
@@ -1,5 +1,9 @@
+#!/usr/bin/env bash
+
 php artisan down --message="Probíhá aktualizace zpěvníku na novou verzi. Zkuste to později" --retry=60
+composer install
 composer dump-auto
+npm run production
 php artisan config:cache
 php artisan cache:clear
 php artisan view:clear
