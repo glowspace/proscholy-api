f0c593c9fa899a2503a4bc9175565da52ae50e7e
Michael Dojčár <michaeldojcar@gmail.com>
2019-02-18 12:59:31 +0100

    deployment bash git npm removed

--- production_deploy.sh
+++ production_deploy.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-git pull
 php artisan down --message="Probíhá aktualizace zpěvníku na novou verzi. Zkuste to později" --retry=60
+
+git pull
 composer install
 composer dump-auto
-npm run production
+#npm run production
 php artisan config:cache
 php artisan cache:clear
 php artisan view:clear
