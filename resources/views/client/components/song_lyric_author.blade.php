dd5516efc29b9467e2e08bd5d324d82270d6f35e
Michael Dojčár <michaeldojcar@gmail.com>
2019-02-19 12:25:16 +0100

    author refactor

--- resources/views/client/components/song_lyric_author.blade.php
+++ resources/views/client/components/song_lyric_author.blade.php
@@ -3,5 +3,9 @@
 @elseif($song_l->authors()->count() == 1)
     <span>Autor: {!! $song_l->authors()->first()->getLink()!!}</span>
 @elseif($song_l->authors()->count() > 1)
-    <span>Autoři {{$song_l}}: {{$song_l->authors->pluck('name')->implode(', ') }}</span>
+    <span>Autoři:
+        @foreach($song_l->authors as $author)
+            {!! $author->getLink() !!}{{ $loop->last ? '' : ', ' }}
+        @endforeach
+    </span>
 @endif
