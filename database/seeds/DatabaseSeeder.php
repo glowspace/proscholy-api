35fc8249670074948be41a2015569bc3f8d386cc
Miroslav Sery <athes01@gmail.com>
2019-02-21 16:05:14 +0100

    basic playing with tests

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,7 +13,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             SongSeeder::class,
-            AuthorSeeder::class
+            AuthorSeeder::class,
+            UserSeeder::class
         ]);
     }
 }
