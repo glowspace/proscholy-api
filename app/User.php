1ef0e7f3609225a68d5311c6407b286f0ef22896
Miroslav Sery <athes01@gmail.com>
2019-02-21 20:56:11 +0100

    installed permission package REQUIRED composer update and migration

--- app/User.php
+++ app/User.php
@@ -5,6 +5,7 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Spatie\Permission\Traits\HasRoles;
 
 /**
  * App\User
@@ -35,7 +36,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
  */
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable, HasRoles;
 
     /**
      * The attributes that are mass assignable.
