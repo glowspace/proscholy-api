5576ceb6d3b8dfaf3601ec07a592d987cec4158a
Miroslav Sery <athes01@gmail.com>
2019-02-23 19:40:25 +0100

    updated admin todo to show files

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -8,6 +8,7 @@ use App\Song;
 use App\SongbookRecord;
 use App\SongLyric;
 use App\External;
+use App\File;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Input;
 
@@ -31,76 +32,11 @@ class AdminController extends Controller
     public function renderTodo()
     {
         return view('admin.todo', [
-            'externals'                     => External::where('author_id', null)->orWhere('song_lyric_id', null)->get(),
+            'externals'                     => External::where('author_id', null)->where('has_anonymous_author', 0)->orWhere('song_lyric_id', null)->get(),
             'songs_w_author'                => SongLyric::whereDoesntHave('authors')->where('has_anonymous_author', 0)->get(),
             'songbook_record_w_translation' => SongbookRecord::where('song_lyric_id', '')->get(),
             'song_lyrics_w_lyrics'          => SongLyric::where('lyrics', '=', null)->get(),
+            'files'                         => File::where('author_id', null)->where('has_anonymous_author', 0)->get()
         ]);
     }
-
-    // public function renderTodoRandom()
-    // {
-    //     $externals_w_author = External::where('author_id', '')->orWhere('song_lyric_id', '')->get();
-    //     $songs_w_author     = Song::whereDoesntHave('authors')->get()->concat(SongLyric::whereDoesntHave
-    //     ('authors')->get()->where('is_original', '0'))->shuffle();
-
-    //     $songbook_record_w_translation = SongbookRecord::where('song_lyric_id', '')->get();
-    //     $song_lyrics_w_lyrics          = SongLyric::where('lyrics', '')->get()->shuffle();
-
-    //     if (
-    //         $externals_w_author->count() == 0 && $songs_w_author->count() == 0 && $songbook_record_w_translation->count() == 0
-    //         && $song_lyrics_w_lyrics->count() == 0
-    //     )
-    //     {
-    //         return redirect()->route('admin.todo');
-    //     }
-
-
-    //     $rand = rand(1, 3);
-
-    //     // Songy bez autora
-    //     if ($rand == 1)
-    //     {
-    //         if ($songs_w_author->count() == 0)
-    //         {
-    //             return redirect()->route('admin.todo.random');
-    //         }
-
-    //         return view('admin.todo_editors.song_w_author', [
-    //             'song'    => $songs_w_author->first(),
-    //             'authors' => Author::all(),
-    //         ]);
-    //     }
-    //     elseif ($rand == 2)
-    //     {
-    //         if ($songbook_record_w_translation->count() == 0)
-    //         {
-    //             return redirect()->route('admin.todo.random');
-    //         }
-
-    //         return view('admin.todo_editors.songbook_record_w_translation', [
-    //             'record'       => $songbook_record_w_translation->first(),
-    //             'translations' => SongLyric::all(),
-    //         ]);
-    //     }
-    //     elseif ($rand == 4)
-    //     {
-    //         // Pokud už žádný není - hurá, vyhledáme něco jinýho.
-    //         if ($song_lyrics_w_lyrics->count() == 0)
-    //         {
-    //             return redirect()->route('admin.todo.random');
-    //         }
-
-    //         return view('admin.todo_editors.translation_w_lyrics', ['translation' => $song_lyrics_w_lyrics->first()]);
-    //     }
-    //     else
-    //     {
-    //         if ($externals_w_author->count() == 0)
-    //         {
-    //             return redirect()->route('admin.todo.random');
-    //         }
-
-    //         return view('admin.todo_editors.external', ['external' => $externals_w_author->first()]);
-    //     }
-    // }
 }
