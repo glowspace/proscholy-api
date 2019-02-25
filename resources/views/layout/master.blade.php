adf6bbd613688165985e739df2003edde5f6cfee
Michael Dojčár <michaeldojcar@gmail.com>
2019-02-25 23:07:01 +0100

    mix cache busting

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -7,11 +7,11 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     {{-- Favicon --}}
-    <link rel="apple-touch-icon" sizes="180x180" href="{{mix('img/favicon/apple-touch-icon.png')}}">
-    <link rel="icon" type="image/png" sizes="32x32" href="{{mix('img/favicon/favicon-32x32.png')}}">
-    <link rel="icon" type="image/png" sizes="16x16" href="{{mix('img/favicon/favicon-16x16.png')}}">
-    <link rel="manifest" href="{{mix('img/favicon/site.webmanifest')}}">
-    <link rel="mask-icon" href="{{mix('img/favicon/safari-pinned-tab.svg')}}" color="#5bbad5">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{asset('img/favicon/apple-touch-icon.png')}}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{asset('img/favicon/favicon-32x32.png')}}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{asset('img/favicon/favicon-16x16.png')}}">
+    <link rel="manifest" href="{{asset('img/favicon/site.webmanifest')}}">
+    <link rel="mask-icon" href="{{asset('img/favicon/safari-pinned-tab.svg')}}" color="#5bbad5">
     <meta name="msapplication-TileColor" content="#da532c">
     <meta name="theme-color" content="#292929">
 
