636db79a34066fdaab32eb8aff73e54605a86f9e
Miroslav Sery <athes01@gmail.com>
2019-03-16 21:43:57 +0700

    added db migrations

---
+++ database/migrations/2019_03_16_143807_create_tags_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTagsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tags', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            // type: 0 normal, 1 official
+            $table->unsignedInteger('type')->default(0);
+            $table->unsignedInteger('mother_tag_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tags');
+    }
+}
