7c502173319ad1985d71511709f0b298ca0cd586
Miroslav Sery <athes01@gmail.com>
2019-03-19 09:27:20 +0700

    commenting

--- resources/views/admin/components/menu.blade.php
+++ resources/views/admin/components/menu.blade.php
@@ -56,6 +56,7 @@
     'text' => 'Soubory bez autora/písničky'
 ])
 
+
 <div class="navbar-label material-shadow text-primary">Úprava položek</div>
 
 @include('admin.components.menu-item', [
@@ -95,59 +96,3 @@
         'text' => 'Uživatelé'
     ])
 @endcan
-
-
-{{-- <a class="btn btn-secondary" href="{{route('admin.dashboard')}}">
-    <i class="fas fa-home"></i> Nástěnka
-</a> --}}
-{{-- <a class="btn btn-secondary" href="{{route('client.home')}}">
-    <i class="fas fa-arrow-left"></i> Návrat na web
-</a>
-<a class="btn btn-secondary" href="{{route('auth.logout')}}">
-    <i class="fas fa-sign-out-alt"></i> Odhlásit se
-</a> --}}
-
-
-{{-- <a class="btn btn-secondary" href="{{route('admin.song.no-lyric')}}">
-    <i class="fas fa-music"></i> <span>Písně bez textu</span>
-</a>
-
-<a class="btn btn-secondary" href="{{route('admin.song.no-author')}}">
-    <i class="fas fa-music"></i> <span>Písně bez autora</span>
-</a>
-
-<a class="btn btn-secondary" href="{{route('admin.song.no-chord')}}">
-    <i class="fas fa-music"></i> <span>Písně bez akordů</span>
-</a>
-
-<a class="btn btn-secondary" href="{{route('admin.external.no-author')}}">
-    <i class="fas fa-link"></i> <span>Odkazy bez autora/písničky</span>
-</a>
-
-<a class="btn btn-secondary" href="{{route('admin.file.no-author')}}">
-    <i class="fas fa-file"></i> <span>Soubory bez autora/písničky</span>
-</a> --}}
-
-{{-- <div class="navbar-label material-shadow text-primary">Úprava položek</div>
-
-<a class="btn btn-secondary" href="{{route('admin.song.index')}}">
-    <i class="fas fa-music"></i> Písně
-</a>
-
-<a class="btn btn-secondary" href="{{route('admin.author.index')}}">
-    <i class="fas fa-pen"></i> Autoři
-</a>
-
-<a class="btn btn-secondary" href="{{route('admin.external.index')}}">
-    <i class="fas fa-link"></i> Externí zdroje
-</a>
-
-<a class="btn btn-secondary" href="{{route('admin.file.index')}}">
-    <i class="fas fa-file"></i> Soubory
-</a>
-
-@can('manage users')
-    <a class="btn btn-secondary" href="{{route('admin.user.index')}}">
-        <i class="fas fa-user"></i> Uživatelé
-    </a>
-@endcan --}}
