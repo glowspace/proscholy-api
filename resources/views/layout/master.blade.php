3e806fd388c31c662291eb9e20065f763ee0d454
Michael Dojčár <michaeldojcar@gmail.com>
2019-03-21 10:03:21 +0100

    collapsable menu

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=0">
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     {{-- Favicon --}}
@@ -42,7 +43,11 @@
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-94042414-8"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+
         gtag('js', new Date());
 
         gtag('config', 'UA-94042414-8');
@@ -54,11 +59,12 @@
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark static-top">
         <div>
-        <img src="{{asset('img/logo_v2.png')}}" width="60" style="padding: 0 10px 0 0">
-        <a class="navbar-brand mr-1" href="@yield('navbar-brand_href', '/')"> Zpěvník pro scholy</a>
+            <img src="{{asset('img/logo_v2.png')}}" width="60" style="padding: 0 10px 0 0;">
+            <a class="navbar-brand mr-1" href="@yield('navbar-brand_href', '/')"> Zpěvník pro scholy</a>
         </div>
 
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation" onclick="toggleNavbar()">
             <span class="navbar-toggler-icon"></span>
         </button>
 
@@ -67,7 +73,7 @@
     <div class="container-fluid" id="app">
         <div class="row">
             {{-- Side navbar --}}
-            <div class="d-sm-block sidebar bg-dark material-shadow" id="navbarNav">
+            <div class="sidebar bg-dark material-shadow" id="navbarNav">
                 @yield('navbar')
             </div>
 
@@ -90,6 +96,35 @@
             viewport.attr("content", "height=" + viewheight + "px, width=" +
                 viewwidth + "px, initial-scale=1.0");
         }, 300);
+
+
+        // Navbar toggling
+        let navbarState = false;
+
+        function toggleNavbar() {
+            console.log(navbarState);
+
+            if (navbarState === false) {
+                showNavbar();
+            }
+            else {
+                hideNavbar();
+            }
+        }
+
+        function showNavbar() {
+            navbarState = true;
+
+            $('.sidebar')
+                .show()
+                .css({position: 'fixed'});
+        }
+
+        function hideNavbar() {
+            navbarState = false;
+
+            $('.sidebar').hide();
+        }
     </script>
 
     @stack('scripts')
