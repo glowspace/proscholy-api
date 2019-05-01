d5a7101379b848d66a8c743cc54c263650a155e5
Miroslav Sery <athes01@gmail.com>
2019-05-01 02:16:27 +0200

    vuetify included in public app.js

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -23,6 +23,57 @@ Vue.component('chord', require('./components/Chord.vue'));
 Vue.component('transposition', require('./components/Transposition.vue'));
 Vue.component('font-sizer', require('./components/FontSizer.vue'));
 
+
+var base_url = document.querySelector('#baseUrl').getAttribute('value');
+
+// HTTP connexion to the API
+const httpLink = createHttpLink({
+  // You should use an absolute URL here
+  uri: base_url + '/graphql',
+})
+
+const cache = new InMemoryCache()
+
+// Create the apollo client
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache,
+})
+
+import VueApollo from 'vue-apollo'
+Vue.use(VueApollo)
+
+const apolloProvider = new VueApollo({
+    defaultClient: apolloClient,
+})
+
+import Vuetify, {
+  VApp, // required
+//   VNavigationDrawer,
+//   VDataTable,
+//   VContainer,
+//   VLayout,
+//   VFlex,
+//   VCard,
+//   VCardText,
+//   VTextField
+} from 'vuetify/lib'
+
+Vue.use(Vuetify, {
+  components: {
+    VApp,
+    // VNavigationDrawer,
+    // VDataTable,
+    VContainer,
+    VLayout,
+    VFlex,
+    VCard,
+    VCardText,
+    // VTextField
+  }
+})
+
 const app = new Vue({
-    el: '#app'
+    el: '#app',
+    apolloProvider
 });
