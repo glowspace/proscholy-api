5416e33bc712d3d1136caae0ff17bc234173b6a4
Michael Dojčár <michaeldojcar@gmail.com>
2019-05-01 10:33:54 +0200

    song list frontend

--- resources/assets/js/components/Search/SongsList.vue
+++ resources/assets/js/components/Search/SongsList.vue
@@ -1,61 +1,63 @@
 <template>
-    
+
     <table class="table">
-        <tr v-for="song_lyric in song_lyrics" v-bind:key="song_lyric.id">
+        <tr v-for="song_lyric in song_lyrics"
+            v-bind:key="song_lyric.id">
+            <td style="width: 15px"><i class="fas fa-music"></i></td>
+
             <td>
-                {{ song_lyric.name }}
-            </td>
-            <!-- <td style="width: 15px"><i class="fas fa-music"></i></td>
-            <td>
-                <a href="{{ $song_lyric->public_url }}">{{$song_lyric->getSearchTitle()}}</a>
+                <a :href="song_lyric.public_url">{{ song_lyric.name }}</a> -
+                <span v-for="(author, index) in song_lyric.authors">
+                    {{ author.name }}<span v-if="index !== song_lyric.authors.length - 1">, </span>
+                </span>
             </td>
 
             <td style="width: 10px;"
                 class="no-left-padding">
-                @if($song_lyric->spotifyTracks()->count())
-                    <i class="fab fa-spotify text-success"></i>
-                @else
-                    <i class="fab fa-spotify text-very-muted"></i>
-                @endif
+                <i v-if="song_lyric.spotifyTracks.length > 0"
+                   class="fab fa-spotify text-success"
+                   title="Tato píseň má nahrávku na Spotify."></i>
+                <i v-else
+                   class="fab fa-spotify text-very-muted"></i>
             </td>
             <td style="width: 10px;"
                 class="no-left-padding">
-                @if($song_lyric->soundcloudTracks()->count())
-                    <i class="fab fa-soundcloud"
-                        style="color: orangered;"></i>
-                @else
-                    <i class="fab fa-soundcloud text-very-muted"></i>
-                @endif
+                <i v-if="song_lyric.soundcloudTracks.length > 0"
+                   class="fab fa-soundcloud"
+                   style="color: orangered;"
+                   title="Tato píseň má nahrávku na Soundcloud.com"></i>
+                <i v-else
+                   class="fab fa-soundcloud text-very-muted"></i>
             </td>
             <td style="width: 10px;"
                 class="no-left-padding">
-                @if($song_lyric->scoreExternals()->count() || $song_lyric->scoreFiles()->count())
-                    <i class="fa fa-file-pdf"
-                        style="color: #3961ad"></i>
-                @else
-                    <i class="fa fa-file-pdf text-very-muted"
-                        style="color: #3961ad"></i>
-                @endif
+                <i v-if="song_lyric.scoreFiles.length > 0"
+                   class="fa fa-file-pdf"
+                   style="color: #3961ad"
+                   title="K této písni jsou k dispozici noty."></i>
+                <i v-else
+                   class="fa fa-file-pdf text-very-muted"></i>
             </td>
             <td style="width: 10px;"
                 class="no-left-padding">
-                @if($song_lyric->youtubeVideos()->count())
-                    <i class="fab fa-youtube text-danger"></i>
-                @else
-                    <i class="fab fa-youtube text-very-muted"></i>
-                @endif
-            </td> -->
+                <i v-if="song_lyric.youtubeVideos.length > 0"
+                   class="fab fa-youtube text-danger"
+                   title="Tato píseň má video na YouTube."></i>
+                <i v-else
+                   class="fab fa-youtube text-very-muted"></i>
+            </td>
         </tr>
-        <!-- <tr>
+
+        <tr :v-if="song_lyrics">
             <td>
                 <i>Žádná píseň s tímto názvem nebyla nalezena.</i>
             </td>
-        </tr> -->
+        </tr>
     </table>
 </template>
 
 <script>
-    import { store } from "./store.js";
+    import {store} from "./store.js";
     import gql from 'graphql-tag';
 
     const fetch_items = gql`
@@ -68,7 +70,8 @@
                 scoreFiles{id},
                 youtubeVideos{id},
                 spotifyTracks{id},
-                soundcloudTracks{id}
+                soundcloudTracks{id},
+                authors{id, name}
             }
         }`;
 
@@ -84,10 +87,10 @@
         },
 
         apollo: {
-            song_lyrics: { 
+            song_lyrics: {
                 query: fetch_items,
                 variables() {
-                    return { 
+                    return {
                         // has_lyrics: this.hasLyrics,
                         // has_authors: this.hasAuthors,
                         // has_chords: this.hasChords,
@@ -97,8 +100,6 @@
             }
         },
 
-        methods: {
-
-        }
+        methods: {}
     }
 </script>
