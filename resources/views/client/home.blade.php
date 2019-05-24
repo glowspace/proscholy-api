4ac7fc537822863f5c55670c2da390c382e5388d
Miroslav Sery <athes01@gmail.com>
2019-05-24 23:39:09 +0200

    homepage scrolling down

--- resources/views/client/home.blade.php
+++ resources/views/client/home.blade.php
@@ -86,4 +86,166 @@
             </div>
         </div>
     </div>
+
+    <div class="home-bottom">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12">
+
+                    <h1>Redakce ProScholy.cz</h1>
+            
+                    <p>Jsme skupina lidí, kteří chtějí usnadnit práci všem,
+                        kteří pracují s českou nebo slovenskou křesťanskou tvorbou.
+                        V dnešní době, kdy jsou materiály i potřebné know-how roztroušeny po mnoha různých místech
+                        na internetu nebo v papírových sbornících a zpěvnících,
+                        chceme nabídnout rychlou a praktickou alternativu.
+                    </p>
+            
+                    <p>
+                        Chcete se podílet na tvorbě zpěvníku?
+                        Máte tipy na nové funkce nebo nový repertoár?
+                        Můžete nám napsat na email <a href="mailto:redakce@proscholy.cz">redakce@proscholy.cz</a> nebo se na nás obrátit telefonicky na <b>734 791 909</b>.</p>
+            
+                    <h3>Náš tým</h3>
+            
+                    <div class="row">
+                        <div class="col-sm-3">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title">Michael Dojčár</h5>
+                                    <p class="card-text">celková koordinace projektu, IT</p>
+                                </div>
+                            </div>
+                        </div>
+            
+                        <div class="col-sm-3">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title">Miroslav Šerý</h5>
+                                    <p class="card-text">vedoucí vývojář</p>
+                                </div>
+                            </div>
+                        </div>
+            
+                        <div class="col-sm-3">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title">Jana Stuchlíková</h5>
+                                    <p class="card-text">redakční tým</p>
+                                </div>
+                            </div>
+                        </div>
+            
+                        <div class="col-sm-3">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title">Mgr. Ondřej Talaš</h5>
+                                    <p class="card-text">koordinace, duchovní podpora</p>
+                                </div>
+                            </div>
+                        </div>
+            
+                        <div class="col-sm-3">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title">Mag. art. Ondřej Múčka</h5>
+                                    <p class="card-text">redakční tým</p>
+                                </div>
+                            </div>
+                        </div>
+            
+                        <div class="col-sm-3">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title">Matěj Kulišťák</h5>
+                                    <p class="card-text">redakční tým</p>
+                                </div>
+                            </div>
+                        </div>
+            
+                        <div class="col-sm-3">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title">Barbora Kuchaříková</h5>
+                                    <p class="card-text">redakční tým</p>
+                                </div>
+                            </div>
+                        </div>
+            
+                        <div class="col-sm-3">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title">Terezie Kološová</h5>
+                                    <p class="card-text">redakční tým</p>
+                                </div>
+                            </div>
+                        </div>
+            
+                        <div class="col-sm-3">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title">Zuzana Haikerová</h5>
+                                    <p class="card-text">redakční tým</p>
+                                </div>
+                            </div>
+                        </div>
+            
+                        <div class="col-sm-3">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title">Václav Šablatura</h5>
+                                    <p class="card-text">redakční tým, odborný konzultant</p>
+                                </div>
+                            </div>
+                        </div>
+            
+                        <div class="col-sm-3">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title">Benjamín Tichý</h5>
+                                    <p class="card-text">grafika</p>
+                                </div>
+                            </div>
+                        </div>
+            
+                        <div class="col-sm-3">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title">Petra Kalousková</h5>
+                                    <p class="card-text">PR tým</p>
+                                </div>
+                            </div>
+                        </div>
+            
+                        <div class="col-sm-3">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title">Emma Kasanová</h5>
+                                    <p class="card-text">PR tým</p>
+                                </div>
+                            </div>
+                        </div>
+            
+                        <div class="col-sm-3">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title">Josef Řídký</h5>
+                                    <p class="card-text">mobilní aplikace</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-sm-3">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title">Vít Kološ</h5>
+                                    <p class="card-text">vývoj a návrh web. rozhraní</p>
+                                </div>
+                            </div>
+                            </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
