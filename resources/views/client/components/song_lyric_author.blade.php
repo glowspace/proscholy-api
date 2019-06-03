ca4735c359a46a9732c1c60b6e26114b7c59e97f
vitkolos <vit.kolos@gmail.com>
2019-06-03 18:55:09 +0200

    song lyric author unified and improved

--- resources/views/client/components/song_lyric_author.blade.php
+++ resources/views/client/components/song_lyric_author.blade.php
@@ -1,30 +1,39 @@
 @php
     $authors_count = $song_l->authors()->count();
     $original_lyric = $song_l->song->getOriginalSongLyric();
+    if($original_lyric !== NULL) {
+        $original_authors_count = $original_lyric->authors()->count();
+    }
+    $author_a = ($original_lyric !== NULL)?"a":"A";
 @endphp
 
 @if($song_l->type !== 0) {{-- translation --}}
-
-    <span>
-        @if($authors_count == 0)
-            Autor překladu: neznámý
+    @if($original_lyric !== NULL)
+        Originál: <a href="{{ $original_lyric->public_url }}">{{ $original_lyric->name }}</a><br>
+        @if($original_authors_count == 0)
+            Autor: neznámý,
         @else
-            @if($authors_count == 1) Autor @else Autoři @endif překladu:
-            
-            @foreach($song_l->authors as $author)
-                <a href="{{route('client.author', $author)}}">{{$author->name}}</a>@if (!$loop->last), @endif
+            @if($original_authors_count == 1) Autor: @else Autoři: @endif
+                    
+            @foreach($original_lyric->authors as $author)
+                <a href="{{route('client.author', $author)}}">{{$author->name}}</a>,
             @endforeach
         @endif
-        @if($original_lyric !== NULL), originál: <a
-                    href="{{ $original_lyric->public_url }}">{{ $original_lyric->name }}</a>
-        @endif
-    </span>
-
-@else {{-- original --}}
-
+    @endif
 
     @if($authors_count == 0)
-        <span>Autor: neznámý</span>
+        {{ $author_a }}utor překladu: neznámý
+    @else
+        @if($authors_count == 1) {{ $author_a }}utor @else {{ $author_a }}utoři @endif překladu:
+        
+        @foreach($song_l->authors as $author)
+            <a href="{{route('client.author', $author)}}">{{$author->name}}</a>@if (!$loop->last), @endif
+        @endforeach
+    @endif
+
+@else {{-- original --}}
+    @if($authors_count == 0)
+        Autor: neznámý
     @else
         @if($authors_count == 1) Autor: @else Autoři: @endif
                 
