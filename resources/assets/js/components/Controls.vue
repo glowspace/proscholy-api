f205c900dfdc411215afbabba846855f89694a71
vitkolos <vit.kolos@gmail.com>
2019-06-07 19:45:45 +0200

    improved media control

--- resources/assets/js/components/Controls.vue
+++ resources/assets/js/components/Controls.vue
@@ -1,7 +1,7 @@
 <template>
 		<div class="controls fixed-bottom position-sticky p-1" v-bind:class="{'bg-light border-top': controlsDisplay}">
 			<tools v-if="toolsDisplay && controlsDisplay"></tools>
-			<media v-if="mediaDisplay && controlsDisplay"></media>
+			<media v-bind:class="[(mediaDisplay && controlsDisplay)?'d-block':'d-none']"></media>
 			<control-buttons v-if="controlsDisplay"></control-buttons>
 			<a class="btn btn-secondary float-right" v-on:click="controlsDisplay=!controlsDisplay"><i class="fas pr-0" v-bind:class="[controlsDisplay?'fa-chevron-right':'fa-chevron-left']"></i></a>
 		</div>
--- resources/assets/js/components/Media.vue
+++ resources/assets/js/components/Media.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="card d-block mb-1 overflow-auto">
+    <div class="card mb-1 overflow-auto">
 		<a class="btn btn-secondary float-right" v-on:click="mediaDisplay=false"><i class="fas fa-times pr-0"></i></a>
 		Tady patří veškerá multimédia (videa z YouTube i ostatní nahrávky) ve formě karet.
     </div>
