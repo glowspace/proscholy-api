2fa367e0e9e6a3f2e7634c35e66f337959842cb1
vitkolos <vit.kolos@gmail.com>
2019-06-08 11:03:21 +0200

    removed br

--- resources/assets/js/components/Tools.vue
+++ resources/assets/js/components/Tools.vue
@@ -2,7 +2,7 @@
     <div class="card d-block mb-1 p-1 overflow-auto">
 		<a class="btn btn-secondary float-right" v-on:click="toolsDisplay=false"><i class="fas fa-times pr-0"></i></a>
 		<transposition></transposition>
-		<font-sizer></font-sizer><br>
+		<font-sizer></font-sizer>
     </div>
 </template>
 
