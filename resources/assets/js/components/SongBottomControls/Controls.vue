7dd3d33de8d3ca82bfad0e1a66c804c15ea92685
vitkolos <vit.kolos@gmail.com>
2019-06-10 19:15:14 +0200

    "tools switched – youtube reloads" bug fixed

--- resources/assets/js/components/SongBottomControls/Controls.vue
+++ resources/assets/js/components/SongBottomControls/Controls.vue
@@ -1,6 +1,6 @@
 <template>
 		<div class="controls fixed-bottom position-sticky p-1" v-bind:class="{'card-footer': controlsDisplay}">
-			<tools v-if="toolsDisplay && controlsDisplay"></tools>
+			<tools v-bind:class="[(toolsDisplay && controlsDisplay)?'d-block':'d-none']"></tools>
 			<media v-bind:class="[(mediaDisplay && controlsDisplay)?'d-block':'d-none']"><slot name="media"></slot></media>
 			<control-buttons v-if="controlsDisplay" :hasmediaslot="hasMediaSlot"></control-buttons>
 			<a class="btn btn-secondary float-right" v-on:click="controlsToggle"><i class="fas pr-0" v-bind:class="[controlsDisplay?'fa-chevron-right':'fa-chevron-left']"></i></a>
--- resources/assets/js/components/SongBottomControls/Tools.vue
+++ resources/assets/js/components/SongBottomControls/Tools.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="card d-block mb-1 p-1 overflow-auto">
+    <div class="card mb-1 p-1 overflow-auto">
 		<a class="btn btn-secondary float-right" v-on:click="toolsDisplay=false"><i class="fas fa-times pr-0"></i></a>
 		<transposition></transposition>
 		<font-sizer></font-sizer>
