b9492deb013522efaee52c2fb91801741732dce3
Miroslav Sery <athes01@gmail.com>
2019-06-20 18:35:55 +0200

    added capo to admin songlyric edit

---
+++ resources/assets/js/admin/components/NumberInput.vue
@@ -0,0 +1,42 @@
+<template>
+    <v-text-field
+        type="number"
+        append-outer-icon="add" @click:append-outer="increment" 
+        prepend-icon="remove" @click:prepend="decrement"
+        :label="label"
+        v-model="number"
+        :data-vv-name="vvName"
+        :error-messages="errors.collect(vvName)"
+    ></v-text-field>
+</template>
+
+<script>
+export default {
+    props: ['value', 'min-value', 'max-value', 'vv-name', 'label'],
+
+    computed: {
+        number: {
+            get() {
+                return this.value;
+            },
+            set(val) {
+                this.$emit("input", val);
+            }
+        }
+    },
+
+    methods: {
+        increment() {
+            if (this.number + 1 <= this.maxValue) {
+                this.number += 1;
+            }
+        },
+
+        decrement() {
+            if (this.number - 1 >= this.minValue) {
+                this.number -= 1;
+            }
+        }
+    }
+}
+</script>
