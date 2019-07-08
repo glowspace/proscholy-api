914361fcefa187bc5a9da5255519880c2dfff197
vitkolos <vit.kolos@gmail.com>
2019-07-08 14:31:01 +0200

    font cutting repaired

--- resources/assets/js/pages/Song/SongView.vue
+++ resources/assets/js/pages/Song/SongView.vue
@@ -71,9 +71,9 @@
           </div>
         </div>
 
-        <div class="card-body py-2">
+        <div class="card-body py-2 pl-3">
           <div class="d-flex justify-content-between">
-              <div id="song-lyrics" style="overflow-y: hidden;">
+              <div id="song-lyrics" class="px-1 overflow-hidden">
                 <!-- here goes the song lyrics (vue components generated as a string by Laravel) -->
                 <slot></slot>
               </div>
