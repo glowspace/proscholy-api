21d6f2fe44bc3915afb1a98ace09563ad0b4d723
vitkolos <vit.kolos@gmail.com>
2019-07-12 18:02:10 +0200

    vyhledavani = home

--- resources/views/client/home.blade.php
+++ resources/views/client/home.blade.php
@@ -1,29 +1,11 @@
 @extends('layout.client')
 
-@section('wrapper-classes', 'home')
+@section('wrapper-classes', 'home home-init')
 
 @section('content')
     <div class="background-home">
-        <div class="container-fluid">
-            <div class="logo-wrapper">
-                <div class="logo"></div>
-                <span class="caption noselect">Zpěvník</span>
-            </div>
-            <div class="search-wrapper">
-                <form method="POST"
-                      action="{{route('client.search')}}">
-                    @csrf
-                    <input class="search-home"
-                           name="query"
-                           placeholder="Zadejte název písně (třeba Ať požehnán je Bůh)"
-                           :autofocus="'autofocus'"
-                           type="search">
-                    <button type="submit"
-                            class="search-submit">
-                        <i class="fa fa-search"></i>
-                    </button>
-                </form>
-            </div>
+        <div class="container">
+            <Search></Search>
         </div>
     </div>
 @endsection
