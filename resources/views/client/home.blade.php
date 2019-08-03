80bdef9b63260330059b07bb48b3f9528cd17cb4
Miroslav Sery <athes01@gmail.com>
2019-08-03 23:12:38 +0200

    safer href attribute

--- resources/views/client/home.blade.php
+++ resources/views/client/home.blade.php
@@ -4,8 +4,8 @@
 
 @push('head_links')
     {{-- preload the logo font --}}
-    <link rel="preload" href="fonts/cocogoose-pro-regular.ttf" as="font" type="font/ttf" crossorigin>
-    <link rel="preload" href="/img/logo_bubble.svg" as="image">
+    <link rel="preload" href="{{ asset('/fonts/cocogoose-pro-regular.ttf')}} " as="font" type="font/ttf" crossorigin>
+    <link rel="preload" href="{{ asset('/img/logo_bubble.svg') }}" as="image">
 @endpush
 
 @section('content')
