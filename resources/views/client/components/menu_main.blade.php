9cd2a783f9030cc7763146f3bd370da17fb22a8d
Miroslav Sery <athes01@gmail.com>
2019-08-09 01:44:15 +0200

    better navbar

--- resources/views/client/components/menu_main.blade.php
+++ resources/views/client/components/menu_main.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark justify-content-between absolute-top">
     <div class="container">
-    <a class="navbar-brand" href="{{url("")}}"><img src="{{asset('img/logo_v2.png')}}" style="padding: 0 10px 0 0;" width="60" alt="logo">Zpěvník pro scholy</a>
+    <a class="navbar-brand" href="{{url("") }}"><img src="{{asset('img/logo_v2.png')}}" style="padding: 0 10px 0 0;" width="60" alt="logo">Zpěvník pro scholy</a>
     @if (Auth::check())
             <a class="navbar-text px-3" href="{{route('admin.dashboard')}}">
             Přihlášený uživatel: {{ Auth::user()->name }}
@@ -9,13 +9,13 @@
             @endif
         </a>
     @endif
-        <div class="d-inline-block">
+        <div class="d-inline-flex">
             <a href="{{url("")}}" class="btn btn-secondary"><i class="fas fa-search"></i> Vyhledávání</a>
             {{-- <a href="#" class="btn btn-secondary"><i class="fas fa-book"></i> Zpěvníky</a> --}}
             {{-- <a href="#" class="btn btn-secondary"><i class="fas fa-star"></i> Oblíbené</a> --}}
-            <a href="{{route("client.about")}}" class="btn btn-secondary"><i class="fas fa-info"></i> O zpěvníku</a>
+            <a href="{{route("client.about")}}" class="btn btn-secondary"><i class="fas fa-info"></i> O&nbsp;zpěvníku</a>
             {{-- <a href="#" class="btn btn-secondary"><i class="fas fa-cog"></i> Nastavení</a> --}}
-            <dark-mode-button text=" Tmavý mód"></dark-mode-button>
+            <dark-mode-button v-cloak> Tmavý&nbsp;mód</dark-mode-button>
         </div>
     </div>
 </nav>
