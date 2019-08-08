a78a1333a0529e14ac00944b0aa44d72f2986e9c
Miroslav Sery <athes01@gmail.com>
2019-08-08 15:30:25 +0200

    envoying

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -2,8 +2,9 @@
 
 @setup
     $repository = 'git@gitlab.com:mdojcar/proscholy.cz.git';
-    $releases_dir = '/var/www/html/releases';
     $app_dir = '/var/www/html';
+    $releases_dir = $app_dir.'/releases';
+    $base_dir = '/var/zpevnik';
     $release = date('YmdHis');
     $new_release_dir = $releases_dir .'/'. $release;
 @endsetup
@@ -16,27 +17,48 @@
     update_symlinks
 @endstory
 
+@story('deploy_docker')
+    clone_repository
+@endstory
+
 @task('clone_repository')
-    echo 'Cloning repository'
-    [ -d {{ $releases_dir }} ] || mkdir {{ $releases_dir }}
+    {{-- login to the docker --}}
+    cd {{ $base_dir }}
+    docker-compose exec -T php bash
+
+    {{-- clone git repositiory int oa new folder --}}
     git clone --depth 1 {{ $repository }} {{ $new_release_dir }}
     cd {{ $new_release_dir }}
+    echo $PWD
     git reset --hard master
-@endtask
 
-@task('update_yarn')
+    {{-- link node_modueles, storage, .env from the main directory--}}
     echo 'Linking node_modules directory'
     ln -nfs {{ $app_dir }}/node_modules {{ $new_release_dir }}/node_modules
+    echo "Linking storage directory"
+    rm -rf {{ $new_release_dir }}/storage
+    ln -nfs {{ $app_dir }}/storage {{ $new_release_dir }}/storage
+    echo 'Linking .env file'
+    ln -nfs {{ $app_dir }}/.env {{ $new_release_dir }}/.env
 
-    echo "Updating yarn node_modules folder ({{ $release }})"
-    cd {{ $releases_dir }}
-    yarn install
-@endtask
 
-@task('run_composer')
+    {{-- run composer --}}
     echo "Starting deployment ({{ $release }})"
-    cd {{ $new_release_dir }}
-    composer install --prefer-dist --no-scripts -q -o
+    composer install --optimize-autoloader --no-dev
+    composer dump-auto
+
+    {{-- run yarn --}}
+    yarn install
+    yarn run production
+
+    php artisan config:cache
+    php artisan route:cache
+    php artisan cache:clear
+    php artisan view:clear
+
+    {{-- the end --}}
+    echo 'Linking current release'
+    ln -nfs {{ $new_release_dir }} {{ $app_dir }}/current
 @endtask
 
 @task('update_symlinks')
