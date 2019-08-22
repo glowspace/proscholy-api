f7588a7c7c79fbbdee508bbaf9953ba0e0a67b31
vitkolos <vit.kolos@gmail.com>
2019-08-22 21:02:54 +0200

    report error

--- resources/views/errors/layout.blade.php
+++ resources/views/errors/layout.blade.php
@@ -14,7 +14,13 @@
         <div class="container">
             <div class="error">
 				<h1>Error @yield('code')</h1>
-				@yield('error-description')
+                @yield('error-description')
+                <div class="text-center text-white pt-5">
+                    <a href="https://docs.google.com/forms/d/e/1FAIpQLSfry7CQD0vPpuC_VB7xGR6NUF2WdPUytQwX8KipKoZcIYxbdA/viewform?usp=pp_url&entry.1025781741={{ urlencode(url()->full()) }}&entry.456507920=@yield('code')"
+                        class="btn btn-outline-light display-all-songs font-weight-bold">
+                        <i class="fas fa-exclamation-triangle pr-1"></i> NAHLÁSIT
+                    </a>
+                </div>
 			</div>
         </div>
     </div>
