30b938cb72071d452b0b8518ae7f065f7ab18a2a
vitkolos <vit.kolos@gmail.com>
2019-08-23 21:50:21 +0200

    new list of team members

--- resources/views/client/about_songbook.blade.php
+++ resources/views/client/about_songbook.blade.php
@@ -34,150 +34,74 @@
 
         <h3>Náš tým</h3>
 
-        <div class="row">
-            <div class="col-12 col-sm-6 col-md-4 col-lg-3">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Michael Dojčár</h5>
-                        <p class="card-text">celková koordinace projektu, IT</p>
-                    </div>
-                </div>
-            </div>
+        @php
+            $our_team = array(
+                array('name' => 'Michael Dojčár', 'team1' => 'organizace', 'team2' => 'organizace, vývoj', 'info' => 'celková koordinace projektu, IT'),
+                array('name' => 'Miroslav Šerý', 'team1' => 'organizace', 'team2' => 'vývoj', 'info' => 'vedoucí vývojář, vývoj webu'),
+                array('name' => 'Jana Stuchlíková', 'team1' => 'organizace', 'team2' => 'redakce', 'info' => 'vedoucí redakce'),
+                array('name' => 'Ondřej Talaš', 'team1' => 'organizace', 'team2' => 'public relations', 'info' => 'koordinace, duchovní podpora'),
+                array('name' => 'Ondřej Múčka', 'team1' => 'organizace', 'team2' => 'redakce', 'info' => 'Musica Sacra'),
 
-            <div class="col-12 col-sm-6 col-md-4 col-lg-3">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Miroslav Šerý</h5>
-                        <p class="card-text">vedoucí vývojář</p>
-                    </div>
-                </div>
-            </div>
+                array('name' => 'Miroslav Šerý',    'team1' => 'vývoj', 'team2' => 'organizace', 'info' => 'vedoucí vývojář, vývoj webu'),
+                array('name' => 'Michael Dojčár', 'team1' => 'vývoj', 'team2' => '', 'info' => 'celková koordinace projektu, IT'),
+                array('name' => 'Vít Kološ', 'team1' => 'vývoj', 'team2' => '', 'info' => 'vývoj webu, návrh rozhraní'),
+                array('name' => 'Josef Řídký', 'team1' => 'vývoj', 'team2' => '', 'info' => 'mobilní aplikace pro Android'),
+                array('name' => 'Patrik Dobiáš', 'team1' => 'vývoj', 'team2' => '', 'info' => 'mobilní aplikace pro iOS'),
+                array('name' => 'Benjamín Tichý', 'team1' => 'vývoj', 'team2' => 'grafika', 'info' => 'logo, vizuální styl, návrh rozhraní'),
 
-            <div class="col-12 col-sm-6 col-md-4 col-lg-3">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Jana Stuchlíková</h5>
-                        <p class="card-text">redakční tým</p>
-                    </div>
-                </div>
-            </div>
+                array('name' => 'Ondřej Talaš', 'team1' => 'public relations', 'team2' => 'organizace', 'info' => 'koordinace, duchovní podpora'),
+                array('name' => 'Emma Kasanová', 'team1' => 'public relations', 'team2' => '', 'info' => ''),
+                array('name' => 'Martin Tůma', 'team1' => 'public relations', 'team2' => '', 'info' => ''),
+                array('name' => 'Petra Kalousková', 'team1' => 'public relations', 'team2' => '', 'info' => ''),
+                array('name' => 'Zuzana Haikerová', 'team1' => 'public relations', 'team2' => 'redakce', 'info' => ''),
 
-            <div class="col-12 col-sm-6 col-md-4 col-lg-3">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Mgr. Ondřej Talaš</h5>
-                        <p class="card-text">koordinace, duchovní podpora</p>
-                    </div>
-                </div>
-            </div>
+                array('name' => 'o. Jan Šlégr', 'team1' => 'zástupci z institucí', 'team2' => '', 'info' => 'Liturgická komise ČBK'),
+                array('name' => 'Veronika Lehrlová', 'team1' => 'zástupci z institucí', 'team2' => '', 'info' => 'Sekce pro mládež ČBK'),
 
-            <div class="col-12 col-sm-6 col-md-4 col-lg-3">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Mag. art. Ondřej Múčka</h5>
-                        <p class="card-text">redakční tým</p>
-                    </div>
-                </div>
-            </div>
+                array('name' => 'Jana Stuchlíková', 'team1' => 'redakce', 'team2' => 'organizace', 'info' => 'vedoucí redakce'),
+                array('name' => 'Ondřej Múčka', 'team1' => 'redakce', 'team2' => 'organizace', 'info' => 'Musica Sacra'),
+                array('name' => 'Václav Šablatura', 'team1' => 'redakce', 'team2' => '', 'info' => 'odborný konzultant'),
+                array('name' => 'Anežka Pobořilová', 'team1' => 'redakce', 'team2' => '', 'info' => ''),
+                array('name' => 'Anna Berková', 'team1' => 'redakce', 'team2' => '', 'info' => ''),
+                array('name' => 'Barbora Kuchaříková', 'team1' => 'redakce', 'team2' => '', 'info' => ''),
+                array('name' => 'Dáška Mojžíšová', 'team1' => 'redakce', 'team2' => '', 'info' => ''),
+                array('name' => 'David Indra', 'team1' => 'redakce', 'team2' => '', 'info' => ''),
+                array('name' => 'Eliška Plačková', 'team1' => 'redakce', 'team2' => '', 'info' => ''),
+                array('name' => 'Jakub Soukal', 'team1' => 'redakce', 'team2' => '', 'info' => ''),
+                array('name' => 'Martina Petrůjová', 'team1' => 'redakce', 'team2' => '', 'info' => ''),
+                array('name' => 'Matěj Kulišťák', 'team1' => 'redakce', 'team2' => '', 'info' => ''),
+                array('name' => 'Monika Šinclová', 'team1' => 'redakce', 'team2' => '', 'info' => ''),
+                array('name' => 'Tereza Halaštová', 'team1' => 'redakce', 'team2' => '', 'info' => ''),
+                array('name' => 'Terezie Kološová', 'team1' => 'redakce', 'team2' => '', 'info' => ''),
+                array('name' => 'Terezie Tichá', 'team1' => 'redakce', 'team2' => '', 'info' => ''),
+                array('name' => 'Zuzana Bayerová', 'team1' => 'redakce', 'team2' => '', 'info' => ''),
+                array('name' => 'Zuzana Haikerová', 'team1' => 'redakce', 'team2' => 'public relations', 'info' => ''),
+            );
+            $teams = array('organizace', 'vývoj', 'public relations', 'zástupci z institucí', 'redakce');
+        @endphp
 
-            <div class="col-12 col-sm-6 col-md-4 col-lg-3">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Matěj Kulišťák</h5>
-                        <p class="card-text">redakční tým</p>
+        @foreach ($teams as $team)
+        <div class="our-team">
+            <h4>{{ ucfirst($team) }}</h4>
+            @php
+                $team_members = array_filter($our_team, function ($var) use ($team) {
+                    return ($var['team1'] == $team);
+                });
+            @endphp
+            <div class="card-columns">
+            @foreach ($team_members as $member)
+                <div>
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title {{ ($member['info'].$member['team2']=='')?'mb-0':'' }}">{{ $member['name'] }}</h5>
+                            <h6 class="card-subtitle mb-2 text-muted {{ ($member['team2']=='')?'d-none':'' }}">{{ $member['team2'] }}</h5>
+                            <p class="card-text">{{ $member['info'] }}</p>
+                        </div>
                     </div>
                 </div>
-            </div>
-
-            <div class="col-12 col-sm-6 col-md-4 col-lg-3">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Barbora Kuchaříková</h5>
-                        <p class="card-text">redakční tým</p>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-12 col-sm-6 col-md-4 col-lg-3">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Terezie Kološová</h5>
-                        <p class="card-text">redakční tým</p>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-12 col-sm-6 col-md-4 col-lg-3">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Zuzana Haikerová</h5>
-                        <p class="card-text">redakční tým</p>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-12 col-sm-6 col-md-4 col-lg-3">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Václav Šablatura</h5>
-                        <p class="card-text">redakční tým, odborný konzultant</p>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-12 col-sm-6 col-md-4 col-lg-3">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Benjamín Tichý</h5>
-                        <p class="card-text">grafika</p>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-12 col-sm-6 col-md-4 col-lg-3">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Petra Kalousková</h5>
-                        <p class="card-text">PR tým</p>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-12 col-sm-6 col-md-4 col-lg-3">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Emma Kasanová</h5>
-                        <p class="card-text">PR tým</p>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-12 col-sm-6 col-md-4 col-lg-3">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Josef Řídký</h5>
-                        <p class="card-text">mobilní aplikace Android</p>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-12 col-sm-6 col-md-4 col-lg-3">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Vít Kološ</h5>
-                        <p class="card-text">vývoj a návrh web. rozhraní</p>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-12 col-sm-6 col-md-4 col-lg-3">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Patrik Dobiáš</h5>
-                        <p class="card-text">mobilní aplikace iPhone</p>
-                    </div>
-                </div>
+            @endforeach
             </div>
         </div>
+        @endforeach
     </div>
 @endsection
