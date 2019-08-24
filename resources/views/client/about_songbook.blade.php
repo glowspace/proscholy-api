9a38bd2f77552a9f0482a78df59eac25d1039aef
vitkolos <vit.kolos@gmail.com>
2019-08-24 16:08:44 +0200

    our team

--- resources/views/client/about_songbook.blade.php
+++ resources/views/client/about_songbook.blade.php
@@ -34,68 +34,66 @@
 
         @php
             $our_team = array(
-                array('name' => 'Michael Dojčár', 'team1' => 'organizace', 'team2' => 'organizace, vývoj', 'info' => 'celková koordinace projektu, IT'),
-                array('name' => 'Miroslav Šerý', 'team1' => 'organizace', 'team2' => 'vývoj', 'info' => 'vedoucí vývojář, vývoj webu'),
-                array('name' => 'Jana Stuchlíková', 'team1' => 'organizace', 'team2' => 'redakce', 'info' => 'vedoucí redakce'),
-                array('name' => 'Ondřej Talaš', 'team1' => 'organizace', 'team2' => 'public relations', 'info' => 'koordinace, duchovní podpora'),
-                array('name' => 'Ondřej Múčka', 'team1' => 'organizace', 'team2' => 'redakce', 'info' => 'Musica Sacra'),
+                array('name' => 'Michael Dojčár', 'team' => 'organizace', 'info' => 'celková koordinace projektu, IT'),
+                array('name' => 'Miroslav Šerý', 'team' => 'organizace', 'info' => 'vedoucí vývojář, vývoj webu'),
+                array('name' => 'Jana Stuchlíková', 'team' => 'organizace', 'info' => 'vedoucí redakce'),
+                array('name' => 'P. Ondřej Talaš', 'team' => 'organizace', 'info' => 'koordinace, duchovní podpora, PR'),
+                array('name' => 'Ondřej Múčka', 'team' => 'organizace', 'info' => 'Musica Sacra'),
                 
-                array('name' => 'Miroslav Šerý',    'team1' => 'vývoj', 'team2' => 'organizace', 'info' => 'vedoucí vývojář, vývoj webu'),
-                array('name' => 'Michael Dojčár', 'team1' => 'vývoj', 'team2' => '', 'info' => 'celková koordinace projektu, IT'),
-                array('name' => 'Vít Kološ', 'team1' => 'vývoj', 'team2' => '', 'info' => 'vývoj webu, návrh rozhraní'),
-                array('name' => 'Josef Řídký', 'team1' => 'vývoj', 'team2' => '', 'info' => 'mobilní aplikace pro Android'),
-                array('name' => 'Patrik Dobiáš', 'team1' => 'vývoj', 'team2' => '', 'info' => 'mobilní aplikace pro iOS'),
-                array('name' => 'Benjamín Tichý', 'team1' => 'vývoj', 'team2' => 'grafika', 'info' => 'logo, vizuální styl, návrh rozhraní'),
+                array('name' => 'Miroslav Šerý',    'team' => 'vývoj', 'info' => 'vedoucí vývojář, vývoj webu'),
+                array('name' => 'Michael Dojčár', 'team' => 'vývoj', 'info' => 'celková koordinace projektu, IT'),
+                array('name' => 'Vít Kološ', 'team' => 'vývoj', 'info' => 'vývoj webu, návrh rozhraní'),
+                array('name' => 'Josef Řídký', 'team' => 'vývoj', 'info' => 'mobilní aplikace pro Android'),
+                array('name' => 'Patrik Dobiáš', 'team' => 'vývoj', 'info' => 'mobilní aplikace pro iOS'),
+                array('name' => 'Benjamín Tichý', 'team' => 'vývoj', 'info' => 'logo, vizuální styl, návrh rozhraní'),
                 
-                array('name' => 'Ondřej Talaš', 'team1' => 'public relations', 'team2' => 'organizace', 'info' => 'koordinace, duchovní podpora'),
-                array('name' => 'Emma Kasanová', 'team1' => 'public relations', 'team2' => '', 'info' => ''),
-                array('name' => 'Martin Tůma', 'team1' => 'public relations', 'team2' => '', 'info' => ''),
-                array('name' => 'Petra Kalousková', 'team1' => 'public relations', 'team2' => '', 'info' => ''),
-                array('name' => 'Zuzana Haikerová', 'team1' => 'public relations', 'team2' => 'redakce', 'info' => ''),
+                array('name' => 'P. Ondřej Talaš', 'team' => 'public relations', 'info' => 'koordinace, duchovní podpora, PR'),
+                array('name' => 'Emma Kasanová', 'team' => 'public relations', 'info' => ''),
+                array('name' => 'Martin Tůma', 'team' => 'public relations', 'info' => ''),
+                array('name' => 'Petra Kalousková', 'team' => 'public relations', 'info' => ''),
+                array('name' => 'Zuzana Haikerová', 'team' => 'public relations', 'info' => ''),
                 
-                array('name' => 'o. Jan Šlégr', 'team1' => 'zástupci z institucí', 'team2' => '', 'info' => 'Liturgická komise ČBK'),
-                array('name' => 'Veronika Lehrlová', 'team1' => 'zástupci z institucí', 'team2' => '', 'info' => 'Sekce pro mládež ČBK'),
+                array('name' => 'P. Jan Šlégr', 'team' => 'zástupci z institucí', 'info' => 'Liturgická komise ČBK'),
+                array('name' => 'Veronika Lehrlová', 'team' => 'zástupci z institucí', 'info' => 'Sekce pro mládež ČBK'),
                 
-                array('name' => 'Jana Stuchlíková', 'team1' => 'redakce', 'team2' => 'organizace', 'info' => 'vedoucí redakce'),
-                array('name' => 'Ondřej Múčka', 'team1' => 'redakce', 'team2' => 'organizace', 'info' => 'Musica Sacra'),
-                array('name' => 'Václav Šablatura', 'team1' => 'redakce', 'team2' => '', 'info' => 'odborný konzultant'),
-                array('name' => 'Anežka Pobořilová', 'team1' => 'redakce', 'team2' => '', 'info' => ''),
-                array('name' => 'Anna Berková', 'team1' => 'redakce', 'team2' => '', 'info' => ''),
-                array('name' => 'Barbora Kuchaříková', 'team1' => 'redakce', 'team2' => '', 'info' => ''),
-                array('name' => 'Dáška Mojžíšová', 'team1' => 'redakce', 'team2' => '', 'info' => ''),
-                array('name' => 'David Indra', 'team1' => 'redakce', 'team2' => '', 'info' => ''),
-                array('name' => 'Eliška Plačková', 'team1' => 'redakce', 'team2' => '', 'info' => ''),
-                array('name' => 'Jakub Soukal', 'team1' => 'redakce', 'team2' => '', 'info' => ''),
-                array('name' => 'Martina Petrůjová', 'team1' => 'redakce', 'team2' => '', 'info' => ''),
-                array('name' => 'Matěj Kulišťák', 'team1' => 'redakce', 'team2' => '', 'info' => ''),
-                array('name' => 'Monika Šinclová', 'team1' => 'redakce', 'team2' => '', 'info' => ''),
-                array('name' => 'Tereza Halaštová', 'team1' => 'redakce', 'team2' => '', 'info' => ''),
-                array('name' => 'Terezie Kološová', 'team1' => 'redakce', 'team2' => '', 'info' => ''),
-                array('name' => 'Terezie Tichá', 'team1' => 'redakce', 'team2' => '', 'info' => ''),
-                array('name' => 'Zuzana Bayerová', 'team1' => 'redakce', 'team2' => '', 'info' => ''),
-                array('name' => 'Zuzana Haikerová', 'team1' => 'redakce', 'team2' => 'public relations', 'info' => ''),
+                array('name' => 'Jana Stuchlíková', 'team' => 'redakce', 'info' => 'vedoucí redakce'),
+                array('name' => 'Ondřej Múčka', 'team' => 'redakce', 'info' => 'Musica Sacra'),
+                array('name' => 'Václav Šablatura', 'team' => 'redakce', 'info' => 'odborný konzultant'),
+                array('name' => 'Anna Berková', 'team' => 'redakce', 'info' => ''),
+                array('name' => 'Barbora Kuchaříková', 'team' => 'redakce', 'info' => ''),
+                array('name' => 'Dagmar Mojžíšová', 'team' => 'redakce', 'info' => ''),
+                array('name' => 'Eliška Plačková', 'team' => 'redakce', 'info' => ''),
+                array('name' => 'Jakub Soukal', 'team' => 'redakce', 'info' => ''),
+                array('name' => 'Lada Radmacherová', 'team' => 'redakce', 'info' => ''),
+                array('name' => 'Martina Petrůjová', 'team' => 'redakce', 'info' => ''),
+                array('name' => 'Matěj Kulišťák', 'team' => 'redakce', 'info' => ''),
+                array('name' => 'Monika Šinclová', 'team' => 'redakce', 'info' => ''),
+                array('name' => 'Tereza Halaštová', 'team' => 'redakce', 'info' => ''),
+                array('name' => 'Terezie Kološová', 'team' => 'redakce', 'info' => ''),
+                array('name' => 'Terezie Tichá', 'team' => 'redakce', 'info' => ''),
+                array('name' => 'Zuzana Bayerová', 'team' => 'redakce', 'info' => ''),
+                array('name' => 'Zuzana Haikerová', 'team' => 'redakce', 'info' => ''),
             );
             $teams = array('organizace', 'vývoj', 'public relations', 'zástupci z institucí', 'redakce');
             $number_of_members = count(array_unique(array_column($our_team, 'name')));
             @endphp
 
-            <h3>Náš {{ $number_of_members }}členný tým</h3>
+            <h3>Náš tým</h3>
 
             @foreach ($teams as $team)
             <div class="our-team">
                 <h4>{{ ucfirst($team) }}</h4>
                 @php
                 $team_members = array_filter($our_team, function ($var) use ($team) {
-                    return ($var['team1'] == $team);
+                    return ($var['team'] == $team);
                 });
                 @endphp
             <div class="card-columns">
-                @foreach ($team_members as $member)
+                @foreach ($team_members as $key => $member)
                 <div>
-                    <div class="card">
+                    <div class="card{{ (array_search($member['name'], array_column($our_team, 'name')) < $key)?' bg-transparent':'' }}">
                         <div class="card-body">
-                            <h5 class="card-title {{ ($member['info'].$member['team2']=='')?'mb-0':'' }}">{{ $member['name'] }}</h5>
-                            <h6 class="card-subtitle mb-2 text-muted {{ ($member['team2']=='')?'d-none':'' }}">{{ $member['team2'] }}</h5>
+                            <h5 class="card-title {{ ($member['info']=='')?'mb-0':'' }}">{{ $member['name'] }}</h5>
                             <p class="card-text">{{ $member['info'] }}</p>
                         </div>
                     </div>
