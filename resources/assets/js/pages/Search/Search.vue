df7ddeb41dfaf88aa7a78465fde6a9f1dcaca7b2
vitkolos <vit.kolos@gmail.com>
2019-10-26 17:16:29 +0200

    home page bottom links remake

--- resources/assets/js/pages/Search/Search.vue
+++ resources/assets/js/pages/Search/Search.vue
@@ -76,11 +76,16 @@
             <i class="fab fa-apple"></i>
         </div>
         <div v-if="init" class="text-center pt-5 text-white app-download pb-3 h2 mb-0">
-            <a href="http://play.google.com/store/apps/details?id=jozkar.mladez" target="_blank"><i class="fab fa-android"></i></a>
-            <a href="http://itunes.apple.com/app/zpěvník-pro-scholy/id1475375453" target="_blank"><i class="fab fa-apple"></i></a>
-            <span class="btn px-2 font-weight-bold text-white">APLIKACE | SOCIÁLNÍ SÍTĚ</span>
-            <a href="https://www.instagram.com/zpevnik.proscholy.cz/" target="_blank"><i class="fab fa-instagram"></i></a>
-            <a href="https://www.facebook.com/proscholy/" target="_blank"><i class="fab fa-facebook"></i></a>
+            <div>
+                <a href="http://play.google.com/store/apps/details?id=jozkar.mladez" target="_blank"><i class="fab fa-android"></i></a>
+                <span class="btn px-2 font-weight-bold">APLIKACE</span>
+                <a href="http://itunes.apple.com/app/zpěvník-pro-scholy/id1475375453" target="_blank"><i class="fab fa-apple"></i></a>
+            </div>
+            <div>
+                <a href="https://www.facebook.com/proscholy/" target="_blank"><i class="fab fa-facebook"></i></a>
+                <span class="btn px-2 font-weight-bold">SOCIÁLNÍ SÍTĚ</span>
+                <a href="https://www.instagram.com/zpevnik.proscholy.cz/" target="_blank"><i class="fab fa-instagram"></i></a>
+            </div>
         </div>
         <a class="btn btn-secondary mb-0 search-report bg-transparent" target="_blank" title="Nahlásit"
         :href="'https://docs.google.com/forms/d/e/1FAIpQLScmdiN_8S_e8oEY_jfEN4yJnLq8idxUR5AJpFmtrrnvd1NWRw/viewform?usp=pp_url&entry.1025781741=' + currentUrl()">
@@ -113,13 +118,13 @@ body {
     right: 0;
 }
 
-.app-download a {
+.app-download div * {
     color: white !important;
 }
 
-.app-download .btn:nth-of-type(2) {
-    background-color: transparent;
-    cursor: initial;
+.app-download span.btn {
+    background-color: transparent !important;
+    cursor: initial !important;
 }
 
 .btn.search-report {
