c9b5ec3a322624f6213de8fc2670adadcbba6aa0
vitkolos <vit.kolos@gmail.com>
2019-10-26 18:01:17 +0200

    minor admin styling changes

--- resources/views/client/components/menu_main.blade.php
+++ resources/views/client/components/menu_main.blade.php
@@ -1,14 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark justify-content-between absolute-top">
     <div class="container">
     <a class="navbar-brand" href="{{url("") }}"><img src="{{asset('img/logo_v2.png')}}" style="padding: 0 10px 0 0;" width="60" alt="logo">Zpěvník pro scholy</a>
-    @if (Auth::check())
-        <a class="navbar-text px-3 user-logged-in-btn" href="{{route('admin.dashboard')}}">
-            <i class="fas fa-user"></i> {{ Auth::user()->name }}
-        @if (Auth::user()->roles()->count() > 0)
-            ({{Auth::user()->roles()->first()->name}})
-        @endif
-        </a>
-    @endif
     <div class="d-inline-flex">
         <a href="{{url("")}}" class="btn btn-secondary"><i class="fas fa-search"></i> Vyhledávání</a>
         {{-- <a href="#" class="btn btn-secondary"><i class="fas fa-book"></i> Zpěvníky</a> --}}
@@ -17,6 +9,14 @@
         {{-- <a href="#" class="btn btn-secondary"><i class="fas fa-cog"></i> Nastavení</a> --}}
         <dark-mode-button v-cloak> Tmavý&nbsp;mód</dark-mode-button>
     </div>
+    @if (Auth::check())
+    <a class="navbar-text px-3 user-logged-in-btn" href="{{route('admin.dashboard')}}">
+        <i class="fas fa-user"></i> {{ Auth::user()->name }}
+    @if (Auth::user()->roles()->count() > 0)
+        ({{Auth::user()->roles()->first()->name}})
+    @endif
+    </a>
+    @endif
     </div>
 </nav>
 
