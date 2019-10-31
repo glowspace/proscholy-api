b990aa40487b683052c65af23b3934a2f9921286
Miroslav Sery <athes01@gmail.com>
2019-10-31 20:36:48 +0100

    working on deploy scripts

---
+++ docker/setup/web/entrypoint_web_prod.sh
@@ -0,0 +1,17 @@
+# # PHP FPM start script
+# cd /var/www/html
+# composer install --ignore-platform-reqs
+# php artisan storage:link
+# php artisan key:generate
+
+# #sleep 30
+# # so far do the import
+# php artisan migrate:fresh --seed
+# php artisan view:clear
+# php artisan config:clear
+# # php artisan vendor:publish --all
+
+# # yarn run watch
+# chmod -R 777 storage
+
+# php-fpm
--- production_deploy.sh
+++ production_deploy.sh
@@ -1,15 +1,36 @@
 #!/usr/bin/env bash
 
-php artisan down --message="Probíhá aktualizace zpěvníku na novou verzi. Zkuste to později" --retry=60
-git reset --hard
-git pull
+LAST=`readlink -f current` 
+LAST=${LAST##*/}
+
+cd releases
+echo 'Removing old releases but one last for backup'
+ls | grep -v ${LAST} | xargs rm -rfv
+
+git clone --depth 1 'git@gitlab.com:proscholy/proscholy.cz.git' /var/www/html/releases/abcd
+cd abcd
+git reset --hard master
+
+echo "Linking storage directory"
+rm -rf storage
+ln -nfs ../../storage storage
+
+mv .env.production .env
+
 composer install --optimize-autoloader --no-dev
 composer dump-auto
+
 yarn install
 yarn run production
+
+rm -rf node_modules
+
+php artisan down --message="Probíhá aktualizace zpěvníku na novou verzi. Zkuste to později" --retry=60
 php artisan config:cache
 php artisan route:cache
 php artisan cache:clear
 php artisan view:clear
 php artisan migrate --force
 php artisan up
+
+ln -nfs . ../../current
