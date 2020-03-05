2544bd481a870ac317f292bfd30a8aa67ba3361c
Michael Dojčár <michaeldojcar@gmail.com>
2020-03-05 21:26:21 +0100

    about songbook styles

--- resources/assets/js/client/about/AboutSongBook.vue
+++ resources/assets/js/client/about/AboutSongBook.vue
@@ -10,6 +10,44 @@
     }
 </script>
 
-<style scoped>
+<style scoped
+       lang="scss">
+
+    @import "~bootstrap/scss/functions";
+    @import "~bootstrap/scss/variables";
+    @import "~bootstrap/scss/mixins";
+
+    .our-team {
+        h4 {
+            transition: padding 0.2s;
+        }
+
+        &:hover h4 {
+            padding-left: 0.5rem;
+        }
+
+        .flex-wrap {
+            margin: 0 -10px;
+
+            .card {
+                flex: 0 0 calc(100% / 3 - 20px);
+                margin-left: 10px;
+                margin-right: 10px;
+
+                @include media-breakpoint-between(sm, md) {
+                    flex: 0 0 calc(100% / 2 - 20px);
+                }
+
+                @include media-breakpoint-down(xs) {
+                    margin-bottom: 15px;
+
+                    .card-body {
+                        padding: 1rem;
+                    }
+                }
+
+            }
+        }
+    }
 
 </style>
