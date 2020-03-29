77fd93250c33c86e3f81b338d6ba1b8b10832ba9
Mira <athes01@gmail.com>
2020-03-29 15:59:12 +0200

    basic graphql tests

---
+++ tests/GraphQL/AuthorTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\GraphQL;
+
+use Tests\CreatesApplication;
+use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Nuwave\Lighthouse\Testing\MakesGraphQLRequests;
+
+use App\Author;
+
+class AuthorTest extends BaseTestCase
+{
+    use CreatesApplication;
+    use MakesGraphQLRequests;
+    use RefreshDatabase;
+ 
+    public function testQueriesAuthors(): void
+    {
+        $author = factory(Author::class)->create();
+
+        $this->graphQL(/** @lang GraphQL */ '
+        {
+            authors {
+                id 
+                name
+                description
+            }
+        }
+        ')->assertJson([
+            'data' => [
+                'authors' => [
+                    [
+                        'id' => $author->id,
+                        'name' => $author->name,
+                        'description' => $author->description,
+                    ]
+                ]
+            ]
+        ]);
+    }
+}
