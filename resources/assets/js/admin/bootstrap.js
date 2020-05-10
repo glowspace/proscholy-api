88fb30afb57c58aaf58732eb1347b79b387d8547
Mira <athes01@gmail.com>
2020-05-10 14:47:14 +0200

    reverted jquery for compatibility reasons

--- resources/assets/js/admin/bootstrap.js
+++ resources/assets/js/admin/bootstrap.js
@@ -9,7 +9,7 @@ window._ = require('lodash');
  */
 
 try {
-    // window.$ = window.jQuery = require('jquery');
+    window.$ = window.jQuery = require('jquery'); 
 
     require('bootstrap-material-design');
 } catch (e) {}
