8988c91351b6bc598ebe8932380ea37d2509ec8e
vitkolos <vit.kolos@gmail.com>
2020-07-29 21:19:19 +0200

    getting datatable and magicsuggest to work

--- resources/views/admin/components/datatable.blade.php
+++ resources/views/admin/components/datatable.blade.php
@@ -1,21 +1,22 @@
 @pushonce('head_links:datatable')
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4/dt-1.10.18/fh-3.1.4/r-2.2.2/datatables.min.css"/>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.21/css/dataTables.bootstrap4.min.css" />
 @endpushonce
 
 @pushonce('scripts:datatable')
-    <script type="text/javascript" src="https://cdn.datatables.net/v/bs4/dt-1.10.18/fh-3.1.4/r-2.2.2/datatables.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.21/js/dataTables.bootstrap4.min.js"></script>
 @endpushonce
 
 @push('scripts')
     <script>
         $(document).ready( function () {
-            $("#{{ $table_id }}").DataTable();
-        } );
-
-        $("#{{ $table_id }}").DataTable( {
-            language: {
-                url: '//cdn.datatables.net/plug-ins/1.10.19/i18n/Czech.json'
-            }
+            $("#{{ $table_id }}").DataTable( {
+                pageLength: 50,
+                language: {
+                    url: '//cdn.datatables.net/plug-ins/1.10.19/i18n/Czech.json'
+                }
+            } );
         } );
     </script>
 @endpush
--- resources/views/admin/components/magicsuggest.blade.php
+++ resources/views/admin/components/magicsuggest.blade.php
@@ -30,4 +30,5 @@
 
 @pushonce('scripts:magicsuggest')
     <script src="{{asset('_admin/js/magicsuggest.js')}}"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 @endpushonce
