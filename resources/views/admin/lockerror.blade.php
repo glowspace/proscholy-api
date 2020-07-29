29f05eb7c4b3e04f34c3662e1b2c34d1de1b033c
vitkolos <vit.kolos@gmail.com>
2020-07-29 10:30:53 +0200

    lockerror improved

--- resources/views/admin/lockerror.blade.php
+++ resources/views/admin/lockerror.blade.php
@@ -1,30 +1,21 @@
 @extends('layout.admin')
 
-@section('content')
+@section('title-suffixed', 'Uzamčeno')
+
+@section('content-withmenu')
     <div class="content-padding">
         <div class="row">
             <div class="col-md-6">
-                @if (isset($song_lyric))
-                    <h2>Je třeba chvilku počkat..</h2>
-                    <p>Vypadá to, že píseň {{ $song_lyric->name }} právě upravuje někdo jiný.
-                    <br>Abychom předešli možným problémům, tak je úprava dočasně uzamčená.
-                    <br><br>
-                    <a class="btn btn-outline-primary" href="{{ route('admin.song.edit', $song_lyric) }}">ZKUSIT ZNOVU UPRAVIT</a>
-                    <br><br>
-                    <img src="https://thumbs.gfycat.com/FoolishHonorableArgentinehornedfrog-size_restricted.gif" alt="You shall not pass">
-                    </p>
-                @endif
-
-                @if (isset($songbook))
-                    <h2>Je třeba chvilku počkat..</h2>
-                    <p>Vypadá to, že zpěvník {{ $songbook->name }} právě upravuje někdo jiný.
-                    <br>Abychom předešli možným problémům, tak je úprava dočasně uzamčená.
-                    <br><br>
-                    <a class="btn btn-outline-primary" href="{{ route('admin.songbook.edit', $songbook) }}">ZKUSIT ZNOVU UPRAVIT</a>
-                    <br><br>
-                    <img src="https://thumbs.gfycat.com/FoolishHonorableArgentinehornedfrog-size_restricted.gif" alt="You shall not pass">
-                    </p>
-                @endif
+                <h1 class="h2">Je třeba chvilku počkat…</h1>
+                <p>
+                    Vypadá to, že
+                    {{ isset($song_lyric) ? 'píseň '.$song_lyric->name : (isset($songbook) ? 'zpěvník '.$songbook->name : '' ) }} právě upravuje někdo jiný.
+                    <br>Abychom předešli možným problémům, dočasně jsme režim úprav uzamkli.
+                    <br><br><a class="btn btn-outline-primary"
+                        href="{{ isset($song_lyric) ? route('admin.song.edit', $song_lyric) : (isset($songbook) ? route('admin.songbook.edit', $songbook) : '' ) }}"
+                    >Zkusit znovu</a>
+                    <br><br><img src="https://thumbs.gfycat.com/FoolishHonorableArgentinehornedfrog-size_restricted.gif" alt="You shall not pass">
+                </p>
             </div>
         </div>
     </div>
