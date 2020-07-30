6bf9d2862db7effe58e6ad31e561bed87422a014
vitkolos <vit.kolos@gmail.com>
2020-07-30 18:49:14 +0200

    Revert "dark mode"
    This reverts commit 64995f0ab352d021f864934e5b92915bf1ffce45.

--- resources/views/layout/admin.blade.php
+++ resources/views/layout/admin.blade.php
@@ -19,7 +19,6 @@
                 ProScholy & Regenschori
                 <span style="color: #ffffff3d">– po ruce všem, kteří se chtějí modlit hudbou</span>
             </a>
-            <a class="btn btn-secondary" id="dark-mode-button" onclick="toggleDarkMode();" >{{ $_COOKIE['dark'] == 'true' ? 'Světlý' : 'Tmavý' }} režim</a>
         </div>
     </nav>
 @endsection
@@ -48,12 +47,6 @@
 @section('app-css')
     <link rel="stylesheet" type="text/css" href="{{ mix('css/app.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ mix('_admin/css/admin.css') }}">
-    @if ($_COOKIE['dark'] == 'true')
-    <style>
-        body {background-color: black;}
-        .col-lg-10 {filter: invert(1);}
-    </style>
-    @endif
 @endsection
 
 @section('app-js')
@@ -61,7 +54,7 @@
 @endsection
 
 @push('scripts')
-    <script>
+<script>
         // Mobile viewport soft keyboard fix
         setTimeout(function () {
             var viewheight = $(window).height();
@@ -70,54 +63,4 @@
             viewport.attr("content", "height=" + viewheight + "px, width=" +
                 viewwidth + "px, initial-scale=1.0");
         }, 300);
-
-        function setCookie(cname,cvalue,exdays) {
-            var d = new Date();
-            d.setTime(d.getTime() + (exdays*24*60*60*1000));
-            var expires = "expires=" + d.toGMTString();
-            document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-        }
-
-        function getCookie(cname) {
-            var name = cname + "=";
-            var decodedCookie = decodeURIComponent(document.cookie);
-            var ca = decodedCookie.split(';');
-            for(var i = 0; i < ca.length; i++) {
-                var c = ca[i];
-                while (c.charAt(0) == ' ') {
-                    c = c.substring(1);
-                }
-                if (c.indexOf(name) == 0) {
-                    return c.substring(name.length, c.length);
-                }
-            }
-            return "";
-        }
-
-        function toggleDarkMode() {
-            let dark = false;
-            if (getCookie('dark') === 'true') {
-                dark = true;
-            }
-            setCookie('dark', !dark, 30);
-            checkDarkMode();
-        }
-
-        function checkDarkMode() {
-            let dark = false;
-            if (getCookie('dark') === 'true') {
-                dark = true;
-            }
-            if (dark) {
-                document.getElementsByTagName('body')[0].style.backgroundColor = 'black';
-                document.getElementsByClassName('col-lg-10')[0].style.filter = 'invert(1)';
-                document.getElementById('dark-mode-button').textContent = 'Světlý režim';
-            } else {
-                document.getElementsByTagName('body')[0].style.backgroundColor = '#f8fafc';
-                document.getElementsByClassName('col-lg-10')[0].style.filter = 'invert(0)';
-                document.getElementById('dark-mode-button').textContent = 'Tmavý režim';
-            }
-        }
-        checkDarkMode();
-    </script>
 @endpush
