b747cccd4bac00802f0815f9e697c0b39bec50cc
Mira <athes01@gmail.com>
2020-08-20 14:16:13 +0200

    extended api for counting song visits

---
+++ app/GraphQL/Mutations/VisitSong.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\GraphQL\Mutations;
+
+use App\SongLyric;
+use App\Visit;
+use DB;
+
+class VisitSong
+{
+    public function resolve($rootValue, array $args)
+    {
+        DB::transaction(function () use ($args) {
+            Visit::create([
+                'visitable' => SongLyric::class,
+                'visitable_id' => $args['song_lyric_id'],
+                'is_mobile' => $args['is_mobile'],
+                'visit_type' => $args['visit_type'],
+                'source' => $args['source']
+            ]);
+
+            // type 1 is LONG, meaning the user stayed long enough at the song page, don't count this to song counter
+            if ($args['visit_type'] != 1) {
+                SongLyric::find($args['song_lyric_id'])->increment('visits');
+            }
+        });
+
+        return [
+            "confirmed" => true,
+        ];
+    }
+}
---
+++ database/migrations/2020_08_19_125118_create_visits_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVisitsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('visits', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('visitable');
+            $table->bigInteger('visitable_id');
+            $table->bigInteger('public_user_id')->nullable();
+
+            $table->smallInteger('source')->default(0);
+            $table->smallInteger('visit_type')->default(0);
+            $table->boolean('is_mobile')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('visits');
+    }
+}
