78eca25542234a00fceba8e804c0ce5e2a2ce839
Mira <athes01@gmail.com>
2020-09-24 18:52:54 +0200

    migrate pivots together with files

--- database/migrations/2020_09_23_195146_migrate_files_to_externals.php
+++ database/migrations/2020_09_23_195146_migrate_files_to_externals.php
@@ -18,7 +18,7 @@ class MigrateFilesToExternals extends Migration
         $files = DB::table('files')->select(['*'])->get();
 
         foreach ($files as $f) {
-            DB::table('externals')->insert([
+            $ex_id = DB::table('externals')->insertGetId([
                 'song_lyric_id' => $f->song_lyric_id,
                 'type' => $this->convertFileTypeToExternalType($f->type),
                 'created_at' => $f->created_at,
@@ -28,6 +28,24 @@ class MigrateFilesToExternals extends Migration
                 'caption' => $f->name,
                 'url' => url("soubor/$f->filename")
             ]);
+
+            foreach (DB::table('author_file')->select(['author_id'])->where('file_id', $f->id)->get() as $author_file_pivot) {
+                DB::table('author_external')->insert([
+                    'author_id' => $author_file_pivot->author_id,
+                    'external_id' => $ex_id
+                ]);
+            }
+
+            foreach (DB::table('taggables')->select('tag_id')->where([
+                'taggable_type' => 'App\File',
+                'taggable_id' => $f->id
+            ])->get() as $taggable_pivot) {
+                DB::table('taggables')->insert([
+                    'taggable_type' => 'App\External',
+                    'taggable_id' => $ex_id,
+                    'tag_id' => $taggable_pivot->tag_id
+                ]);
+            }
         }
     }
 
@@ -51,6 +69,13 @@ class MigrateFilesToExternals extends Migration
      */
     public function down()
     {
+        DB::table('taggables')->leftJoin('externals', 'taggable_id', '=', 'externals.id')->where([
+            'is_uploaded' => true,
+            'taggable_type' => 'App\External'
+        ])->delete();
+
+        // author_external pivot deleting is handled by the db foreign key `cascade` constraint
+
         DB::table('externals')->where('is_uploaded', true)->delete();
     }
 }
