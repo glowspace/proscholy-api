4a30a9b683d4070576bf87286eb4c01c14f1931b
Mira <athes01@gmail.com>
2020-09-28 17:28:20 +0200

    chord processor '/4' '/7' 'es' 's' updates

---
+++ tests/Unit/ChordSignTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Helpers\ChordSign;
+
+class ChordSignTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+
+    public function testIs()
+    {
+        $this->assertEquals(
+            'C#',
+            ChordSign::parseFromText('Cis7')->getBase()
+        );
+    }
+
+    public function testEs()
+    {
+        $this->assertEquals(
+            'Db',
+            ChordSign::parseFromText('Des7')->getBase()
+        );
+    }
+
+    public function test47()
+    {
+        $this->assertEquals(
+            '4/7',
+            ChordSign::parseFromText('C#4/7')->getExtension()
+        );
+        $this->assertEquals(
+            '',
+            ChordSign::parseFromText('C#4/7')->getBassNote()
+        );
+    }
+}
