2f00d2be9fcd110e1860120864680e9615b3af81
Mira <athes01@gmail.com>
2020-10-20 11:28:58 +0200

    run stats every ten minutes instead of every minute

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('compute:stats')
-            ->everyMinute()
+            ->everyTenMinutes()
             ->appendOutputTo(storage_path('logs/schedule.log'));
     }
 
