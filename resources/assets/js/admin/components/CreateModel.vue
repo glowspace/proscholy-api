69448c361e50a02dafd8b84563f527b6d51ddef8
vitkolos <vit.kolos@gmail.com>
2020-10-25 23:50:15 +0100

    fancy slash

--- resources/assets/js/admin/components/CreateModel.vue
+++ resources/assets/js/admin/components/CreateModel.vue
@@ -1,47 +1,42 @@
 <template>
     <v-card class="mb-3 px-4 py-3">
         <v-layout row>
-            <v-inline-flex>
-                <v-text-field
-                    :label="label"
-                    required
-                    v-model="attribute_value"
-                    data-vv-name="required_attribute"
-                    :error-messages="errors.collect('required_attribute')"
-                    prepend-icon="add"
-                    @click:prepend="$refs.cmtf.focus()"
-                    ref="cmtf"
-                    class="mt-0 pb-0 pt-3"
-                    style="max-width:600px;width:50vw"
-                    @keydown.enter="submit(true)"
-                    @input="$validator.errors.clear()"
-                    id="create-model-text-field"
-                ></v-text-field>
-            </v-inline-flex>
-            <v-inline-flex>
-                <div class="text-nowrap pt-1">
-                    <v-btn
-                        :disabled="attribute_value == '' || saving"
-                        @click="submit(true)"
-                        color="primary"
-                        style="margin-left:33px"
-                        >Vytvořit a upravit</v-btn
-                    >
-                    <v-btn
-                        v-if="!forceEdit"
-                        :disabled="attribute_value == '' || saving"
-                        @click="submit(false)"
-                        >Vytvořit</v-btn
-                    >
-                </div>
-            </v-inline-flex>
-        </v-layout>
-        <v-layout row v-if="enableFileUpload">
-            <v-flex>
+            <div class="text-nowrap pt-1" v-if="enableFileUpload">
                 <FileUploadDialog
-                    v-on:submit="onFileDialogSubmit"
+                    @submit="onFileDialogSubmit"
                 ></FileUploadDialog>
-            </v-flex>
+                <svg class="text-very-muted mt-2" style="height:3rem;transform:rotate(80deg);" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><path fill="currentColor" d="M594.53 508.63L6.18 53.9c-6.97-5.42-8.23-15.47-2.81-22.45L23.01 6.18C28.43-.8 38.49-2.06 45.47 3.37L633.82 458.1c6.97 5.42 8.23 15.47 2.81 22.45l-19.64 25.27c-5.42 6.98-15.48 8.23-22.46 2.81z"></path></svg>
+            </div>
+            <v-text-field
+                :label="label"
+                required
+                v-model="attribute_value"
+                data-vv-name="required_attribute"
+                :error-messages="errors.collect('required_attribute')"
+                prepend-icon="add"
+                @click:prepend="$refs.cmtf.focus()"
+                ref="cmtf"
+                class="mt-0 pb-0 pt-3"
+                style="max-width:600px;width:50vw"
+                @keydown.enter="submit(true)"
+                @input="$validator.errors.clear()"
+                id="create-model-text-field"
+            ></v-text-field>
+            <div class="text-nowrap pt-1">
+                <v-btn
+                    :disabled="attribute_value == '' || saving"
+                    @click="submit(true)"
+                    color="primary"
+                    style="margin-left:33px"
+                    >Vytvořit a upravit</v-btn
+                >
+                <v-btn
+                    v-if="!forceEdit"
+                    :disabled="attribute_value == '' || saving"
+                    @click="submit(false)"
+                    >Vytvořit</v-btn
+                >
+            </div>
         </v-layout>
     </v-card>
 </template>
