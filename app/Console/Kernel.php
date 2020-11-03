62eb7c0e0afadcc21f70ca7a67c923e7e29490b9
Mira <athes01@gmail.com>
2020-11-03 08:59:46 +0100

    changed interval to stats comp. to 5 minutes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('compute:stats')
-            ->everyTenMinutes()
+            ->everyFiveMinutes()
             ->appendOutputTo(storage_path('logs/schedule.log'));
 
         $schedule->command('backup:run')
