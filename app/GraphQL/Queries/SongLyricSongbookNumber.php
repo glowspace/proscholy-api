915193a9c8836a23e21f586499c5177c87e864b6
Mira <athes01@gmail.com>
2020-11-14 10:29:04 +0100

    added endpoint for getting song with songbook number

---
+++ app/GraphQL/Queries/SongLyricSongbookNumber.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\GraphQL\Queries;
+
+use App\SongLyric;
+
+use App\Helpers\SongPart;
+use Log;
+
+class SongLyricSongbookNumber
+{
+	public function resolve($rootValue, array $args)
+	{
+		$match_songbook_shortcut_lazy = "([\d\w]+?)";
+		$match_number = "(\d+\w*)";
+
+		preg_match("/$match_songbook_shortcut_lazy\s*$match_number/", $args['number'], $matches);
+
+		if (count($matches) == 0) {
+			return;
+		}
+
+		return SongLyric::whereHas('songbook_records', function ($q) use ($matches) {
+			return $q->where('number', $matches[2])
+				->where('shortcut', $matches[1]);
+		})->first();
+	}
+}
