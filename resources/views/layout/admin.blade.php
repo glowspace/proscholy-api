5756a1e72e457bd022ef5b19bdc574372dffa0e0
Michael Dojčár <michaeldojcar@gmail.com>
2020-12-01 15:48:54 +0100

    Revert "ZPS-60 make admin blade standalone (remove master layout extension)"
    This reverts commit 4ef6d942

--- resources/views/layout/admin.blade.php
+++ resources/views/layout/admin.blade.php
@@ -1,105 +1,75 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport"
-          content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=0">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+@extends('layout.master')
 
-    {{-- Favicon --}}
-    <link rel="apple-touch-icon" sizes="180x180" href="{{asset('img/favicon/apple-touch-icon.png')}}">
-    <link rel="icon" type="image/png" sizes="32x32" href="{{asset('img/favicon/favicon-32x32.png')}}">
-    <link rel="icon" type="image/png" sizes="16x16" href="{{asset('img/favicon/favicon-16x16.png')}}">
-    <link rel="manifest" href="{{asset('img/favicon/site.webmanifest')}}">
-    <link rel="mask-icon" href="{{asset('img/favicon/safari-pinned-tab.svg')}}" color="#5bbad5">
-    <meta name="msapplication-TileColor" content="#da532c">
-    <meta name="theme-color" content="#292929">
-    <meta id="baseUrl" name="baseUrl" value="{{url('')}}">
-    <meta id="userToken" name="userToken" value="{{ Auth::check() ? Auth::user()->getApiToken() : ''}}">
+@if(View::hasSection('title-edit'))
+    @section('title')
+    ● @yield('title-edit') – administrace ProScholy & Regenschori
+    @endsection
+@elseif(View::hasSection('title-suffixed'))
+    @section('title')
+    @yield('title-suffixed') – administrace ProScholy & Regenschori
+    @endsection
+@else
+    @section('title', 'Administrace ProScholy & Regenschori')
+@endif
 
-    @stack('meta_tags')
+@section('navbar')
+    <nav class="navbar navbar-admin navbar-expand-lg navbar-dark justify-content-between absolute-top">
+        <div>
+            <a class="navbar-brand" href="{{route('admin.dashboard')}}"><img src="{{asset('img/logo_v2.png')}}" class="admin-logo">
+                ProScholy & Regenschori
+                <span class="navbar-motto">– po ruce všem, kteří se chtějí modlit hudbou</span>
+            </a>
+            <a class="btn btn-secondary" id="dark-mode-button" onclick="toggleDarkMode();" ><i class="fas fa-{{ (isset($_COOKIE['dark']) && $_COOKIE['dark'] == 'true') ? 'sun' : 'moon' }}"></i> {{ (isset($_COOKIE['dark']) && $_COOKIE['dark'] == 'true') ? 'Světlý' : 'Tmavý' }} režim</a>
+        </div>
+    </nav>
+@endsection
 
-    <title>
-        @if(View::hasSection('title-edit'))
+@section('content')
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-2">
+                <div class="sidebar bg-dark material-shadow">
+                    <div>
+                        <a class="navbar-brand py-2" href="{{route('admin.dashboard')}}"><img src="{{asset('img/logo_v2.png')}}" class="admin-logo"></a>
+                        @include('admin.components.menu')
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-10" style="position: static">
+                @yield('content-withmenu')
+            </div>
+        </div>
+    </div>
+    <a class="btn btn-secondary mb-0 admin-report bg-transparent" target="_blank" title="Nahlásit chybu" href="https://slack.com/app_redirect?team=TCC9MSFQA&channel=CGHL024DD">
+        <i class="fas fa-exclamation-triangle p-0"></i>
+    </a>
+@endsection
 
-            ● @yield('title-edit') – Regenschori administrace
-
-        @elseif(View::hasSection('title-suffixed'))
-
-            @yield('title-suffixed') – Regenschori administrace
-
-        @else
-            Regenschori - administrace
-        @endif
-    </title>
-
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
-
-    <!-- Import Google Icon Font -->
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-
-    <!-- Fonts awesome -->
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
-
-    {{-- CSS --}}
+@section('app-css')
     <link rel="stylesheet" type="text/css" href="{{ mix('css/app.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ mix('_admin/css/admin.css') }}">
     @if (isset($_COOKIE['dark']) && $_COOKIE['dark'] == 'true')
-        <style id="darkStyle">
-            .theme--light {opacity: 0; transition: .7s;}
-            .theme--dark {opacity: 1; transition: .7s;}
-        </style>
-        <script>
-            var dom_observer = new MutationObserver(function(mutation) {
-                // this runs (multiple times but most importantly), before the body is rendered
-                document.getElementsByTagName('body')[0].setAttribute('class', 'dark admin-dark');
-            });
-            var container = document.documentElement || document.body;
-            var config = { attributes: true, childList: true, characterData: true };
-            dom_observer.observe(container, config);
-        </script>
+    <style id="darkStyle">
+        .theme--light {opacity: 0; transition: .7s;}
+        .theme--dark {opacity: 1; transition: .7s;}
+    </style>
+    <script>
+        var dom_observer = new MutationObserver(function(mutation) {
+            // this runs (multiple times but most importantly), before the body is rendered
+            document.getElementsByTagName('body')[0].setAttribute('class', 'dark admin-dark');
+        });
+        var container = document.documentElement || document.body;
+        var config = { attributes: true, childList: true, characterData: true };
+        dom_observer.observe(container, config);
+    </script>
     @endif
+@endsection
 
-    @yield('google-analytics')
-
-    @stack('head_links')
-</head>
-<body>
-    <div id="app" class="@yield('wrapper-classes', 'page')">
-        <nav class="navbar navbar-admin navbar-expand-lg navbar-light justify-content-between absolute-top">
-            <div>
-                <a class="navbar-brand" href="{{route('admin.dashboard')}}"><img src="{{asset('img/logo_v2.png')}}" class="admin-logo">
-                    Regenschori Administrace
-                </a>
-                <a class="btn btn-secondary" id="dark-mode-button" onclick="toggleDarkMode();" ><i class="fas fa-{{ (isset($_COOKIE['dark']) && $_COOKIE['dark'] == 'true') ? 'sun' : 'moon' }}"></i> {{ (isset($_COOKIE['dark']) && $_COOKIE['dark'] == 'true') ? 'Světlý' : 'Tmavý' }} režim</a>
-            </div>
-        </nav>
-
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-lg-2">
-                    <div class="sidebar">
-                        <div>
-                            <a class="navbar-brand py-2" href="{{route('admin.dashboard')}}"><img src="{{asset('img/logo_v2.png')}}" class="admin-logo"></a>
-                            @include('admin.components.menu')
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-10" style="position: static">
-                    @yield('content-withmenu')
-                </div>
-            </div>
-        </div>
-        <a class="btn btn-secondary mb-0 admin-report bg-transparent" target="_blank" title="Nahlásit chybu" href="https://slack.com/app_redirect?team=TCC9MSFQA&channel=CGHL024DD">
-            <i class="fas fa-exclamation-triangle p-0"></i>
-        </a>
-    </div>
-
-    {{-- Main JS built with Laravel's mix --}}
+@section('app-js')
     <script type="text/javascript" src="{{ mix('_admin/js/app.js') }}"></script>
+@endsection
 
+@push('scripts')
     <script>
         // mobile viewport soft keyboard fix
         setTimeout(function () {
@@ -163,9 +133,4 @@
             document.getElementById('darkStyle').remove();
         }
     </script>
-    @stack('scripts')
-</body>
-</html>
-
-
-
+@endpush
