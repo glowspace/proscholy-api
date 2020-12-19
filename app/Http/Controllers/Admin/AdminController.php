a81d30b7554421265dcf0abf65a6d8d7c75e85d1
Michael Dojčár <michaeldojcar@gmail.com>
2020-12-19 12:23:43 +0100

    ZPS-107 added more metrics to dashboard

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -10,20 +10,32 @@ use App\SongLyric;
 use App\External;
 use App\File;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Input;
+
 
 class AdminController extends Controller
 {
     public function renderDash()
     {
         return view('admin.dash', [
-            'songs_count'        => SongLyric::count(),
+            'songs_count' => SongLyric::count(),
             'songs_w_text_count' => SongLyric::where('lyrics', '!=', '')->count(),
+            'songs_w_chords_count' => SongLyric::where('has_chords', '=', true)->count(),
+
+            'songs_w_score_count' => SongLyric::whereHas('externals', function ($q) { # Song has score in external
+                # External must be score
+                $q->where('content_type', '2');
+            })
+                ->orWhere('lilypond', '!=', null) # or song has lilypond score
+                ->count(),
+
             'songs_w_all_count' => SongLyric::where('lyrics', '!=', '')
                 ->where('has_chords', '=', true)
                 ->where(function ($query) {
-                    $query->whereHas('authors', null)->orWhere('has_anonymous_author', 1);
-                })->whereHas('tags')->count(),
+                    $query->whereHas('authors', null)
+                        ->orWhere('has_anonymous_author', 1);
+                })->whereHas('tags')
+                ->count(),
+
             'songs_w_just_title_count' => SongLyric::whereNull('lyrics')
                 ->where(function ($query) {
                     $query->whereDoesntHave('authors', null)->where('has_anonymous_author', 0);
@@ -31,9 +43,11 @@ class AdminController extends Controller
                 ->whereDoesntHave('tags', null)
                 ->whereDoesntHave('songbook_records', null)
                 ->whereDoesntHave('files', null)
-                ->whereDoesntHave('externals')->count(),
-            'authors_count'      => Author::count(),
-            'externals_count'    => External::count(),
+                ->whereDoesntHave('externals')
+                ->count(),
+
+            'authors_count' => Author::count(),
+            'externals_count' => External::count(),
         ]);
     }
 }
