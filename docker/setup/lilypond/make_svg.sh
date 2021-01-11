a6eff8b2e0ff03f63fed6250b0bb3f90bc80f5e9
Mira <athes01@gmail.com>
2021-01-11 21:07:28 +0100

    playing with svg/pdf generating

--- docker/setup/lilypond/make_svg.sh
+++ docker/setup/lilypond/make_svg.sh
@@ -8,3 +8,10 @@ if lilypond -dbackend=eps -dno-point-and-click --format=eps a.ly 2>err.txt ; the
 else
     cat err.txt
 fi
+
+
+# lilypond -dbackend=eps -dno-point-and-click --format=eps a.ly 
+
+# lilypond -dbackend=eps -dno-point-and-click -dgs-load-fonts --format=eps a.ly
+
+# cp *.otf /usr/local/lilypond/usr/share/lilypond/current/fonts/otf/
