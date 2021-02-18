0ea4dc572c28a96b906adecfa1aef21db27c829c
Mira <athes01@gmail.com>
2021-02-18 13:15:43 +0100

    ZPS-269 fixed crappy cropping improvements

--- docker/setup/lilypond/scripts/svg_bbox.py
+++ docker/setup/lilypond/scripts/svg_bbox.py
@@ -13,6 +13,6 @@ b=int(vals[2])/5
 c=int(vals[3])/5
 d=int(vals[4])/5
 
-print(" ".join(str(x) for x in [a, -d+2, (c-a)+1, (d-b)+2]))
+print(" ".join(str(x) for x in [a, -d, (c-a)+1, (d-b)+1]))
 
 # output: 25.4 3.8 68.19999999999999 48.6
