b324a7c9eca5945cfa9466138438877662556005
Mira <athes01@gmail.com>
2021-05-25 22:18:04 +0200

    fixed visit issues

--- app/GraphQL/Mutations/VisitSong.php
+++ app/GraphQL/Mutations/VisitSong.php
@@ -10,7 +10,7 @@ class VisitSong
     public function resolve($rootValue, array $args)
     {
         Visit::create([
-            'visitable' => SongLyric::class,
+            'visitable_type' => SongLyric::class,
             'visitable_id' => $args['song_lyric_id'],
             'is_mobile' => $args['is_mobile'] ?? null,
             'visit_type' => $args['visit_type'],
