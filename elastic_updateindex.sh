08deee631b2ea16092f328393e63c5c8ec9fa388
Mira <athes01@gmail.com>
2022-04-04 12:48:15 +0200

    updated elastic scripting

--- elastic_updateindex.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-NEW_UUID=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 4 | head -n 1)
-
-php artisan elastic:migrate "App\SongLyric" song_lyric_${NEW_UUID}
-php artisan elastic:migrate "App\Author" author_${NEW_UUID}
