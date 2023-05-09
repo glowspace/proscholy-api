b2f4233f4c5657a29f290e2f1d4e652f41db9b31
Mira <athes01@gmail.com>
2023-05-09 18:42:33 +0200

    backup: set up google drive client

--- config/backup.php
+++ config/backup.php
@@ -96,7 +96,7 @@ return [
              * The disk names on which the backups will be stored.
              */
             'disks' => [
-                'dropbox',
+                'local',
             ],
         ],
 
